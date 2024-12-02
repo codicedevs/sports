7807837ecfb0ec42a2ad40d2f073ef0421ec608f
StalaDev <siminicnicolas@gmail.com>
2024-12-02 11:05:12 -0300

    fonts

--- mobile/navigation/appNavigator.tsx
+++ mobile/navigation/appNavigator.tsx
@@ -4,9 +4,18 @@ import VersionModal from '../components/modal/versionModal'
 import { useSession } from '../context/authProvider'
 import SplashScreen from '../screens/splash'
 import { AuthStackScreen, Principal } from './stacks'
+import { useFonts } from 'expo-font'
 
 const AppNavigator = () => {
     const { currentUser } = useSession()
+
+    const [loaded, error] = useFonts({
+        'coolvetica': require('../assets/fonts/coolvetica.otf')
+    });
+
+    if (!loaded) {
+        return null;
+    }
 
     return (
         <NavigationContainer>
