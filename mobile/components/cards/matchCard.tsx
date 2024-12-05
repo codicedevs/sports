fd8446565b71b3965180950ad8f53d9a1d93d9b0
falgarania <algarania.franco@gmail.com>
2024-12-05 17:26:11 -0300

    casi terminada card partido (matchCard.tsx)

--- mobile/components/cards/matchCard.tsx
+++ mobile/components/cards/matchCard.tsx
@@ -1,3 +1,4 @@
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { ImageBackground } from "react-native";
 import { Div, Text } from "react-native-magnus";
@@ -10,65 +11,135 @@ const MatchCard = () => {
       w={"95%"}
       flexDir="row"
       rounded={"lg"}
-      bg="black"
+      bg="grey"
       shadow={"2xl"}
       shadowColor="gray"
       overflow="hidden"
+      ml={10}
     >
       {/* Parte izquierda */}
-      <Div
-        flex={2}
-        justifyContent="center"
-        alignItems="center"
-        roundedLeft={"lg"}
-        bg="red"
-        p={10}
+
+      <ImageBackground
+        source={require("../../assets/cesped1.jpg")}
+        style={{
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        imageStyle={{
+          borderTopLeftRadius: 10,
+          borderBottomLeftRadius: 10,
+          opacity: 0.9,
+        }}
       >
-        <Text fontSize={"4xl"} color="white" fontWeight="bold">
+          <LinearGradient
+    colors={["rgba(169, 169, 160, 0)", "rgba(169, 169, 169, 0.7)"]} 
+    style={{
+      position: "absolute",
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      borderTopLeftRadius: 10,
+      borderBottomLeftRadius: 10,
+    }}
+    start={{ x: 0, y: 0 }}
+    end={{ x: 0, y: 1 }}
+  />
+        <Div
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.6)", // Negro semitransparente
+            borderTopLeftRadius: 10,
+            borderBottomLeftRadius: 10,
+          }}
+        />
+        <Text fontSize={35} color="white" fontFamily="RobotoCondensed-Bold">
           2
         </Text>
-        <Text fontSize={"md"} color="white" fontWeight="bold">
+        <Text
+          fontSize={"md"}
+          color="white"
+          fontFamily="RobotoCondensed-Regular"
+        >
           MONDAY
         </Text>
-        <Text fontSize={"sm"} color="white">
+        <Text
+          fontSize={"sm"}
+          color="white"
+          fontFamily="RobotoCondensed-Regular"
+        >
           10:00 PM
         </Text>
-      </Div>
+      </ImageBackground>
 
       {/* Parte derecha */}
-      <Div flex={7} justifyContent="space-between" p={scale(10)}>
-        
+      <Div flex={7} justifyContent="space-between">
+        <LinearGradient
+          colors={[
+            "rgba(20, 20, 20, 8)", // Gris oscuro arriba
+            "rgba(30, 30, 30, 0.7)", // Gris centro
+            "rgba(20, 20, 20, 0.5)",
+          ]}
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%", // Cubre toda la tarjeta
+            top: 0,
+          }}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 1 }}
+        />
         <Div style={{ flex: 1, position: "relative" }}>
           <ImageBackground
-            source={require("../../assets/escudo6.png")}
+            source={require("../../assets/Escudorosariocentral.png")}
             style={{
               position: "absolute",
               top: 0,
-              right: -90, 
-              width: "100%", 
+              right: -90,
+              width: "100%",
               height: "110%",
             }}
             imageStyle={{
-              opacity: 0.2, 
+              opacity: 0.2,
             }}
           />
-          
-          <Div style={{ flex: 1, zIndex: 1, justifyContent: "space-between" }}>
+
+          <Div
+            style={{ flex: 1, zIndex: 1, justifyContent: "space-between" }}
+            p="lg"
+          >
             <Div>
-              <Text fontSize={"lg"} color="white" fontWeight="bold">
+              <Text
+                fontSize={25}
+                color="white"
+                fontFamily="RobotoCondensed-Bold"
+              >
                 NOMBRE PARTIDO
               </Text>
             </Div>
 
-            <Div flexDir="row" justifyContent="space-between" alignItems="center">
-              <Text fontSize={"3xl"} color="white" fontWeight="bold">
+            <Div
+              flexDir="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text
+                fontSize={"3xl"}
+                color="white"
+                fontFamily="RobotoCondensed-Regular"
+              >
                 Detalles
               </Text>
               <Div
                 w={30}
                 h={30}
                 rounded="circle"
-                bg="green500"
+                bg="black"
                 justifyContent="center"
                 alignItems="center"
               >
@@ -76,15 +147,24 @@ const MatchCard = () => {
                   ?
                 </Text>
               </Div>
-              <Text fontSize={"3xl"} color="white" fontWeight="bold">
+              <Text
+                fontSize={"3xl"}
+                color="white"
+                fontFamily="RobotoCondensed-Regular"
+              >
                 Detalles
               </Text>
             </Div>
           </Div>
         </Div>
 
-        <Div bg="black">
-          <Text fontSize={"sm"} color="white">
+        <Div bg="black" p="lg">
+          <Text
+            color="white"
+            fontFamily="RobotoCondensed-Regular"
+            textTransform="uppercase"
+            fontSize={12}
+          >
             Abasto Fútbol 5
           </Text>
         </Div>
