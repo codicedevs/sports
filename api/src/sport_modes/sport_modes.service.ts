b5126ba9abc7761a00c178a63ddd8d0536e0e23a
CatoYuvone <otacyuvo@gmail.com>
2024-12-12 13:47:32 -0300

    [backend] sport_modes service: added findForSports

--- api/src/sport_modes/sport_modes.service.ts
+++ api/src/sport_modes/sport_modes.service.ts
@@ -3,13 +3,13 @@ import { CreateSportModeDto } from './dto/create-sport_mode.dto';
 import { UpdateSportModeDto } from './dto/update-sport_mode.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { SportMode } from './entities/sport_mode.entity';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { FindManyFilter } from 'filter/filter.dto';
 
 @Injectable()
 export class SportModesService {
   constructor(@InjectModel(SportMode.name) private readonly sportModeModel: Model<SportMode>) { }
-  create(createSportModeDto: CreateSportModeDto) {
+  async create(createSportModeDto: CreateSportModeDto) {
     const createdSportMode = new this.sportModeModel(createSportModeDto)
     return createdSportMode.save()
   }
@@ -17,6 +17,11 @@ export class SportModesService {
   async findAll(options?: FindManyFilter<SportMode>): Promise<SportMode[]> {
     const sportsModes = await this.sportModeModel.find(options).exec();
     return sportsModes;
+  }
+
+  async findForSports(sportIds: Types.ObjectId[]): Promise<SportMode[]> {
+    const filter = sportIds ? { sport: { $in: sportIds } } : {};
+    return this.sportModeModel.find(filter).exec();
   }
 
   findOne(id: number) {
