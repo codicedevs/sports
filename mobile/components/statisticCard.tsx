6c8db5c6c5cd0ed4db929f82afb2a2459f0b1182
falgarania <algarania.franco@gmail.com>
2024-12-18 16:42:54 -0300

    Hecha la staticCard

--- mobile/components/statisticCard.tsx
+++ mobile/components/statisticCard.tsx
@@ -1,35 +1,61 @@
 import React from "react";
+import { ImageBackground, StyleSheet } from "react-native";
 import { Div, Text } from "react-native-magnus";
+import { scale } from "react-native-size-matters";
+
 
 const StatisticCard = () => {
   return (
-    <Div w="100%" h="24%" justifyContent="flex-end">
+    <Div w="100%" h={scale(110)} rounded="xl" overflow="hidden" >
       <Div
         flex={8.5}
-        bg="gray500"
+        bg="#5D3C81"
         roundedTop="2xl"
         justifyContent="center"
-        px="lg"
+        px={scale(18)}
       >
-        <Text fontSize="5xl" fontWeight="bold">
-          PREGAME
+        <Text fontSize="6xl" fontFamily="AcuminProCondensed" fontWeight="bold" color="#FFFFFF">
+          TORNEO
         </Text>
-        <Text fontSize="5xl" fontWeight="bold">
-          BREAKDOWN
+        <Text fontSize="6xl" fontFamily="AcuminProCondensed" fontWeight="bold" color="#FFFFFF">
+          VETERANOS
         </Text>
+
+        {/* Imag Copa */}
+        <ImageBackground
+          source={require("../assets/cup1.png")}
+          style={styles.imageBackground}
+          resizeMode="contain"
+        />
       </Div>
+
+      {/* Parte inferior */}
       <Div
         w="100%"
-        bg="blue"
-        flex={1.5}
+        h={scale(35)}
+        bg="#05F3FF"
+        flex={2.3}
         roundedBottom="2xl"
         justifyContent="center"
-        px="lg"
+        px={scale(18)}
       >
-        <Text>BULLS AT MAGIC</Text>
+        <Text fontFamily="RobotoCondensed-Black" color="#5D3C81" fontSize="lg">
+          ARRANCA 05/01
+        </Text>
       </Div>
     </Div>
   );
 };
 
 export default StatisticCard;
+
+const styles = StyleSheet.create({
+  imageBackground: {
+    position: "absolute",
+    right: 0,
+    top: "20%",
+    width: scale(70),
+    height: scale(76),
+    paddingLeft: "29%"
+  },
+});
