22f7ca7754481c164b93c12eee29f50501d35e29
falgarania <algarania.franco@gmail.com>
2024-12-19 14:57:43 -0300

    hecho header.tsx
    corregir fuente de "totito"

--- mobile/components/header.tsx
+++ mobile/components/header.tsx
@@ -1,23 +1,33 @@
-import { Image, StyleProp } from "react-native";
 import { Text } from "react-native-animatable";
 import { Div } from "react-native-magnus";
 
-interface HeaderProps {
-}
+interface HeaderProps {}
 
 export default function Header({ props }: HeaderProps) {
-    return (
-        <Div justifyContent="space-between" flexDir="row" w={"100%"} >
-            <Div>
-                <Text style={{ fontFamily: "RobotoCondensed-Regular", fontSize: 19 }}>Bienvenido</Text>
-                <Text style={{ fontFamily: "RobotoCondensed-Bold", fontSize: 25 }}>Martin</Text>
-            </Div>
-            <Div>
-                <Image
-                    style={{ width: 100, height: 100, marginTop: -25 }}
-                    source={require('@assets/logo.png')}
-                />
-            </Div>
-        </Div>
-    )
+  return (
+    <Div w="100%" px={3} py={5}>
+      <Div>
+        <Text
+          style={{
+            fontFamily: "RobotoCondensed",
+            fontSize: 13,
+            color: "#000",
+          }}
+        >
+          BIENVENIDO
+        </Text>
+      </Div>
+
+      <Text
+        style={{
+          fontFamily: "RobotoCondensed-Black",
+          fontSize: 30,
+          color: "#000",
+          lineHeight: 33,
+        }}
+      >
+        TOTITO
+      </Text>
+    </Div>
+  );
 }
