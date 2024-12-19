b0885cfed0fcddcdba214374685fcc8115809411
falgarania <algarania.franco@gmail.com>
2024-12-19 15:03:55 -0300

    ahora si, hecho el header
    cambiar font de TOTITO

--- mobile/components/header.tsx
+++ mobile/components/header.tsx
@@ -1,33 +1,41 @@
-import { Text } from "react-native-animatable";
+import React from "react";
+import { Text, StyleSheet } from "react-native";
 import { Div } from "react-native-magnus";
 
 interface HeaderProps {}
 
 export default function Header({ props }: HeaderProps) {
   return (
-    <Div w="100%" px={3} py={5}>
+    <Div style={styles.container}>
+      {/* Texto "Bienvenido" */}
       <Div>
-        <Text
-          style={{
-            fontFamily: "RobotoCondensed",
-            fontSize: 13,
-            color: "#000",
-          }}
-        >
-          BIENVENIDO
-        </Text>
+        <Text style={styles.welcomeText}>BIENVENIDO</Text>
       </Div>
 
-      <Text
-        style={{
-          fontFamily: "RobotoCondensed-Black",
-          fontSize: 30,
-          color: "#000",
-          lineHeight: 33,
-        }}
-      >
-        TOTITO
-      </Text>
+      {/* Texto "Totito" */}
+      <Text style={styles.titleText}>TOTITO</Text>
     </Div>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    paddingHorizontal: 3,
+    paddingVertical: 5, 
+  },
+  welcomeText: {
+    fontFamily: "RobotoCondensed",
+    fontSize: 13,
+    color: "#000",
+    textTransform: "uppercase", 
+  },
+  titleText: {
+    fontFamily: "RobotoCondensed-Black",
+    fontSize: 30,
+    color: "#000",
+    lineHeight: 33,
+    textTransform: "uppercase", 
+  },
+});
+
