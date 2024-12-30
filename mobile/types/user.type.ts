46b86539d50f0b72871090c9b672426a4b4d5b8c
StalaDev <siminicnicolas@gmail.com>
2024-12-30 11:58:47 -0300

    new data

--- mobile/types/user.type.ts
+++ mobile/types/user.type.ts
@@ -20,3 +20,22 @@ export interface UserInfo {
     password: string;
     friends: User[];
   }
+
+  // Interfaz principal para los datos de autenticación
+export interface AuthSSOData {
+  idToken: string | null;
+  scopes: string[];
+  serverAuthCode: string | null;
+  user: UserData;
+  type: 'success' | 'error';
+}
+
+// Interfaz para los datos del usuario
+interface UserData {
+  email: string;
+  familyName: string;
+  givenName: string;
+  id: string;
+  name: string;
+  photo: string | null;
+}
