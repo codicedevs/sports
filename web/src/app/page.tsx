9f818b6bff3056a58c72f20ee766c89c81bdf973
dorefici <orefici.diego@gmail.com>
2025-01-07 11:03:37 -0300

    try

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,3 @@
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 import Image from "next/image";
@@ -11,20 +10,23 @@ export async function generateMetadata() {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NzIwZWYwZTNhNzhlYmMxMDU2NGU5NzkiLCJ1c2VybmFtZSI6ImRpZWdvIiwicm9sZXMiOlsidXNlciJdLCJpYXQiOjE3MzYxNzgxNTUsImV4cCI6MTczNjI2NDU1NX0.oyCZhydg-68MWV6zOY3FNkfkNwGPT7jDgd2aUIxU4hE'
-        }
-    }).then((res) => res.json())
+        },
+    }).then(res => res.json());
 
-    // const _parent = await parent
+    console.log(match);
     
+
+    const userNames = match.users.map(user => user.name).join(',');
+
     return {
         title: match.name,
         description: match.location.address,
-        authors: [{ name: match.name }, { name: 'Josh', url: 'https://codice.dev:3000' }],
         openGraph: {
-            images: ['https://via.placeholder.com/600/92c952', '/sports.png'],
+            images: [`/api/image?users=${encodeURIComponent(userNames)}`],
         },
-    }
+    };
 }
+
 
 export default async function Page() {
     const res = await fetch('https://codice.dev:3000/matches/6772c91a790633512b4bdbee', {
@@ -35,7 +37,7 @@ export default async function Page() {
             'Accept': 'application/json',
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NzIwZWYwZTNhNzhlYmMxMDU2NGU5NzkiLCJ1c2VybmFtZSI6ImRpZWdvIiwicm9sZXMiOlsidXNlciJdLCJpYXQiOjE3MzYxNzgxNTUsImV4cCI6MTczNjI2NDU1NX0.oyCZhydg-68MWV6zOY3FNkfkNwGPT7jDgd2aUIxU4hE'
         }
-    }); 
+    });
 
     if (!res.ok) {
         return <div>Error</div>;
@@ -58,25 +60,7 @@ export default async function Page() {
                     {match.name}
                 </>
             )}
-            {/* <main>
-                {match ? (
-                    <Fragment key={match._id}>
-                        <h3 className="font-bold">Partido</h3>
-                        <p>Nombre {match.name}</p>
-                        <p>Fecha {match.date}</p>
-                        <p>Hora {match.hour}</p>
-                        <p>Lugar {match.location.name}</p>
-                        <h3 className="font-bold">Jugadores</h3>
-                        {match.users.map((player: User) => (
-                            <div key={player._id}>
-                                <p>{player.name}</p>
-                            </div>
-                        ))}
-                    </Fragment>
-                ) : (
-                    <p>No users found.</p>
-                )}
-            </main> */}
         </div>
     );
 }
+
