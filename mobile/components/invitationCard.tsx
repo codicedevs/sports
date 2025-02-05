649ebd3c55f32526279fd1c21e7255b67527b9cf
falgarania <algarania.franco@gmail.com>
2025-02-05 14:37:58 -0300

    terminado invitation card

---
+++ mobile/components/invitationCard.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { Div, Text, Button } from "react-native-magnus";
+import { scale } from "react-native-size-matters";
+import { customTheme } from "../utils/theme";
+import { Image } from "react-native";
+
+interface MatchInvitationProps {
+  title: string;
+  matchType: string;
+  date: string;
+  time: string;
+}
+
+const MatchInvitation: React.FC<MatchInvitationProps> = ({
+  title,
+  matchType,
+  date,
+  time,
+}) => {
+  return (
+    <Div
+      bg="black"
+      w={"100%"}
+      h={scale(150)}
+      rounded={customTheme.borderRadius.small}
+      p={customTheme.spacing.medium}
+    >
+      <Text
+        color={customTheme.colors.secondary}
+        fontSize={customTheme.fontSize.medium}
+        fontFamily="NotoSans-Variable"
+      >
+        {title}
+      </Text>
+
+      <Div flexDir="row" alignItems="center">
+        <Div flexDir="row" alignItems="center">
+          <Image source={require("../assets/IconPelota.png")} />
+          <Text
+            color="white"
+            fontSize={customTheme.fontSize.medium}
+            ml={customTheme.spacing.small}
+            mr={customTheme.spacing.medium}
+          >
+            {matchType}
+          </Text>
+        </Div>
+
+        <Image source={require("../assets/iconTime.png")} />
+        <Text
+          color="white"
+          ml={customTheme.spacing.small}
+          fontSize={customTheme.fontSize.medium}
+        >
+          {date}
+        </Text>
+
+        <Div>
+          <Text color="white" fontSize={customTheme.fontSize.medium}>
+            {time}
+          </Text>
+        </Div>
+      </Div>
+
+      <Div flexDir="row" style={{ gap: 20, marginTop: 20 }}>
+        <Button flex={1} bg="black" borderColor="white" borderWidth={1}>
+          <Text
+            color="white"
+            fontFamily="NotoSans-BoldItalic"
+            fontSize={customTheme.fontSize.medium}
+          >
+            Rechazar
+          </Text>
+        </Button>
+        <Button flex={1} bg={customTheme.colors.secondary}>
+          <Text
+            fontFamily="NotoSans-BoldItalic"
+            fontSize={customTheme.fontSize.medium}
+          >
+            Aceptar
+          </Text>
+        </Button>
+      </Div>
+    </Div>
+  );
+};
+
+export default MatchInvitation;
--- mobile/screens/home.tsx
+++ mobile/screens/home.tsx
@@ -1,86 +1,51 @@
 import React from "react";
 import { AppScreenProps, AppScreens } from "../navigation/screens";
-import { Div, Text, Button } from "react-native-magnus";
+import { Div, Text } from "react-native-magnus";
 import { scale } from "react-native-size-matters";
-import { customTheme } from "../utils/theme";
+import MatchInvitation from "../components/invitationCard";
 import { Image } from "react-native";
+import { customTheme } from "../utils/theme";
 
 const HomeScreen: React.FC<AppScreenProps<AppScreens.HOME_SCREEN>> = ({
   navigation,
 }) => {
   return (
-    <Div style={{ flex: 1, padding: 8 }}>
-      <Div
-        bg="black"
-        w={"100%"}
-        h={scale(150)}
-        mt={scale(100)}
-        rounded={customTheme.borderRadius.small}
-        p={customTheme.spacing.medium}
-      >
-        <Text
-          color={customTheme.colors.secondary}
-          fontSize={customTheme.fontSize.medium}
-          fontFamily="NotoSans-Variable"
-        >
-          Ramiro te ha invitado a un partido
-        </Text>
-        <Div flexDir="row" alignItems="center" >
-          <Div flexDir="row" alignItems="center">
-            <Image source={require("../assets/IconPelota.png")} />
-            <Text
-              color="white"
-              fontSize={customTheme.fontSize.medium}
-              mr={customTheme.spacing.medium}
-              ml={customTheme.spacing.small}
-            >
-              Futbol 5
-            </Text>
-          </Div>
-
-          <Image source={require("../assets/iconTime.png")} />
-          <Text
-            color="white"
-            mr={customTheme.spacing.small}
-            ml={customTheme.spacing.small}
-            fontSize={customTheme.fontSize.medium}
-          >
-            Vi 25/01
-          </Text>
-
-          <Div>
-            <Text
-              color="white"
-              mr={customTheme.spacing.medium}
-              fontSize={customTheme.fontSize.medium}
-            >
-              19:00hs
-            </Text>
-          </Div>
-        </Div>
-        <Div
-          flexDir="row"
-          style={{ gap: 20,  marginTop: 20 }}
-        >
-          <Button flex={1} bg="black" borderColor="white" borderWidth={1}>
-            <Text
-              color="white"
-              fontFamily="NotoSans-BoldItalic"
-              fontSize={customTheme.fontSize.medium}
-            >
-              Rechazar
-            </Text>
-          </Button>
-          <Button flex={1} bg={customTheme.colors.secondary}>
-            <Text
-              fontFamily="NotoSans-BoldItalic"
-              fontSize={customTheme.fontSize.medium}
-            >
-              Aceptar
-            </Text>
-          </Button>
-        </Div>
+    <Div>
+      <Div style={{ padding: 10, marginTop: 30 }}>
+        <MatchInvitation
+          title="Ramiro te ha invitado a un partido"
+          matchType="Futbol 5"
+          date="Vi 25/01"
+          time="19:00hs"
+        />
       </Div>
+      {/* <Div
+        style={{
+          padding: 10,
+          marginTop: 20,
+          width: "100%",
+        }}
+      >
+        <Image
+          style={{ width: "100%" }}
+          source={require("../assets/fotoCardTorneo.png")}
+        />
+        <Div bg="black" w="100%" h={scale(30)} flexDir="row" justifyContent="space-between" alignItems="center">
+          <Div flexDir="row" alignItems="center">
+            <Text
+              color="white"
+              fontFamily="NotoSans-BoldItalic"
+              fontSize={customTheme.fontSize.medium}
+            >
+              21/02
+            </Text>
+            <Text color="white">Sumá a tu equipo!</Text>
+          </Div>
+          <Div>
+            <Image source={require("../assets/flechaCardTorneo.png")}></Image>
+          </Div>
+        </Div>
+      </Div> */}
     </Div>
   );
 };
