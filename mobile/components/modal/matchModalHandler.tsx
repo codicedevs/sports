802cf3227419d354ab5b827a584d4f699b3bdf4a
StalaDev <siminicnicolas@gmail.com>
2025-02-05 15:09:13 -0300

    working with animations

--- mobile/components/modal/matchModalHandler.tsx
+++ mobile/components/modal/matchModalHandler.tsx
@@ -5,7 +5,7 @@ import sportService from '../../service/sport.service';
 import useFetch from '../../hooks/useGet';
 import { QUERY_KEYS } from '../../types/query.types';
 import sportmodeService from '../../service/sportmode.service';
-import { ScrollView, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Keyboard, ScrollView, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import locationService from '../../service/location.service';
 import RNDateTimePicker from '@react-native-community/datetimepicker';
@@ -15,8 +15,9 @@ import { AppScreens } from '../../navigation/screens';
 import { navigate } from '../../utils/navigation';
 import Match from '../../types/match.type';
 import { customTheme } from '../../utils/theme';
-import { verticalScale } from 'react-native-size-matters';
-import { MotiView } from 'moti';
+import { scale, verticalScale } from 'react-native-size-matters';
+import { MotiText, MotiView } from 'moti';
+import { interpolateColor } from 'react-native-reanimated';
 
 const mockHours = [
   "10:00",
@@ -38,58 +39,58 @@ const MatchModalHandler = ({ open, setOpen, match }: { open: boolean; setOpen: R
   const { currentUser } = useSession()
 
   // NUEVO
-
   const [openFirst, setOpenFirst] = useState(false)
-  const fetchSportModesById = async () => {
-    if (selectedSport) {
-      if (!match) setSelectedSportMode('')
-      const res = await sportmodeService.getModesBySportId(selectedSport)
-      console.log('POR ID')
-      return res
-    } else {
-      const res = await sportmodeService.getAll()
-      console.log("TODO")
-      return res
-    }
-  }
 
-  const onChangeDate = (event, selectedDate) => {
-    setShowCalendar(false);
+  // const fetchSportModesById = async () => {
+  //   if (selectedSport) {
+  //     if (!match) setSelectedSportMode('')
+  //     const res = await sportmodeService.getModesBySportId(selectedSport)
+  //     console.log('POR ID')
+  //     return res
+  //   } else {
+  //     const res = await sportmodeService.getAll()
+  //     console.log("TODO")
+  //     return res
+  //   }
+  // }
 
-    if (event.type === "set" && selectedDate) {
-      setDate(selectedDate);
-    }
-  };
+  // const onChangeDate = (event, selectedDate) => {
+  //   setShowCalendar(false);
 
-  const onChangeTime = (event, selectedTime) => {
-    setShowTimePicker(false);
+  //   if (event.type === "set" && selectedDate) {
+  //     setDate(selectedDate);
+  //   }
+  // };
 
-    if (event.type === "set" && selectedTime) {
-      setTime(selectedTime);
-    }
-  };
+  // const onChangeTime = (event, selectedTime) => {
+  //   setShowTimePicker(false);
+
+  //   if (event.type === "set" && selectedTime) {
+  //     setTime(selectedTime);
+  //   }
+  // };
 
   // Get info
   const { data: sports } = useFetch(sportService.getAll, [QUERY_KEYS.SPORTS]);
-  const { data: sportModes, isFetching } = useFetch(fetchSportModesById, [QUERY_KEYS.SPORT_MODES, selectedSport]);
+  const { data: sportModes, isFetching } = useFetch(sportmodeService.getAll, [QUERY_KEYS.SPORT_MODES]);
   const { data: locations } = useFetch(locationService.getAll, [QUERY_KEYS.LOCATIONS])
 
-  useEffect(() => {
-    if (match) return
-    if (sports) {
-      setSelectedSport(sports.data[0]._id)
-    }
-  }, [sports])
+  // useEffect(() => {
+  //   if (match) return
+  //   if (sports) {
+  //     setSelectedSport(sports.data[0]._id)
+  //   }
+  // }, [sports])
 
-  useEffect(() => {
-    if (match) {
-      setSelectedSportMode(match.sportMode)
-      return
-    }
-    if (selectedSport && sportModes) {
-      setSelectedSportMode(sportModes.data[0]._id)
-    }
-  }, [sportModes])
+  // useEffect(() => {
+  //   if (match) {
+  //     setSelectedSportMode(match.sportMode)
+  //     return
+  //   }
+  //   if (selectedSport && sportModes) {
+  //     setSelectedSportMode(sportModes.data[0]._id)
+  //   }
+  // }, [sportModes])
 
   const createMatch = async () => {
     try {
@@ -125,45 +126,177 @@ const MatchModalHandler = ({ open, setOpen, match }: { open: boolean; setOpen: R
     }
   }
 
+  const mockSport = [{ name: 'Futbol', _id: '123' }, { name: 'Basquet', _id: '321' }, { name: "padel", id: '434' }]
+  const mockSportMode = [{ name: '5', _id: '123' }, { name: '7', _id: '321' }, { name: "11", id: '434' }]
+
   if (!sports || !sportModes) return null;
   return (
-    // <Overlay visible={open} onBackdropPress={() => setOpen(false)} p="lg" rounded="lg">
     <Modal isVisible={open} onBackButtonPress={() => setOpen(false)}>
       <Div p={customTheme.spacing.medium}>
-        <ScrollView>
+        <ScrollView keyboardShouldPersistTaps="handled" nestedScrollEnabled={true}>
 
           {/* TITULO */}
-          <Div flexDir='row' mt={customTheme.spacing.large} mb={customTheme.spacing.medium}>
+          {/* <Div flexDir='row' mt={customTheme.spacing.large} mb={customTheme.spacing.medium}>
             <Button>X</Button>
             <Text fontSize="md" mb="md" color='black'>
               {match ? 'Editar partido' : 'Organizar partido'}
             </Text>
-          </Div>
-          <TouchableWithoutFeedback onPress={() => setOpenFirst(!openFirst)}>
-          <MotiView
-        from={{ height: verticalScale(50), borderWidth: 1 }}
-        animate={{ height: openFirst ? verticalScale(342) : verticalScale(35), borderWidth: 1 }}
-        transition={{ type: "spring", damping: 15 }}
-        style={{
-          borderColor: "#000", // Ajusta el color del borde si es necesario
-          borderRadius: 10,
-          alignItems: "center",
-          justifyContent: "center",
-          
-        }}
-      >
-        {/* <Div
-          borderWidth={1}
-          p={10}
-          rounded="lg"
-          alignItems="center"
-          justifyContent="center"
-        > */}
-          <Text>Deporte</Text>
-        {/* </Div> */}
-      </MotiView>
-    </TouchableWithoutFeedback>
+          </Div> */}
 
+          <MotiView
+            from={{ height: verticalScale(50), borderWidth: 1 }}
+            animate={{ height: openFirst ? verticalScale(342) : verticalScale(35), borderWidth: 1 }}
+            transition={{ type: "spring", damping: 15 }}
+            style={{
+              borderColor: "#000",
+              borderRadius: 10,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Div
+              flex={1}
+              w={"100%"}
+            >
+              {/* TITULO */}
+              {!openFirst &&
+                <TouchableWithoutFeedback onPress={() => setOpenFirst(true)}>
+                  <Div py={customTheme.spacing.small} h={verticalScale(35)} w={"100%"}>
+                    <Text>Deporte</Text>
+                  </Div>
+                </TouchableWithoutFeedback>
+              }
+              {
+                openFirst &&
+                <>
+                  <Div
+                    py={customTheme.spacing.medium}
+                    style={{ gap: 16 }}>
+                    <Text fontSize={customTheme.fontSize.medium} px={customTheme.spacing.medium}>¿Que deporte juegan?</Text>
+                    <ScrollView showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={{ gap: 16 }}>
+                      {
+                        mockSport.map((sport, index) => (
+                          <TouchableWithoutFeedback onPress={() => setSelectedSport(sport._id)}>
+                            <MotiView
+                              key={index}
+                              style={{
+                                width: scale(112),
+                                height: verticalScale(112),
+                                borderWidth: 1,
+                                borderRadius: 'lg',
+                                justifyContent: 'center',
+                                marginLeft: index === 0 ? 16 : 0,
+                                marginRight: index === mockSport.length - 1 ? 16 : 0,
+                              }}
+                              animate={{
+                                backgroundColor: interpolateColor(
+                                  selectedSport === sport._id ? 1 : 0, // Controlamos la interpolación entre 1 (seleccionado) y 0 (no seleccionado)
+                                  [0, 1], // Rango de la animación
+                                  ['white', 'black'] // Colores entre los que se interpolará el fondo
+                                ),
+                              }}
+                              transition={{
+                                type: 'timing',
+                                duration: 300, // Duración de la animación
+                              }}
+                            >
+                              <MotiText
+                                style={{
+                                  textAlign: 'center',
+                                  fontSize: customTheme.fontSize.medium,
+                                }}
+                                animate={{
+                                  color: interpolateColor(
+                                    selectedSport === sport._id ? 1 : 0, // Mismo control para el texto
+                                    [0, 1], // Rango de la animación
+                                    ['black', 'white'] // Colores entre los que se interpolará el texto
+                                  ),
+                                }}
+                                transition={{
+                                  type: 'timing',
+                                  duration: 300, // Duración de la animación
+                                }}
+                              >
+                                {sport.name}
+                              </MotiText>
+                            </MotiView>
+                          </TouchableWithoutFeedback>
+                        ))
+                      }
+                    </ScrollView>
+                  </Div>
+
+                  <Div px={customTheme.spacing.medium}>
+                    <Div w={'100%'} h={1} borderBottomWidth={1} />
+                  </Div>
+
+                  <Div py={customTheme.spacing.medium} style={{ gap: 16 }}>
+                    <Text fontSize={customTheme.fontSize.medium} px={customTheme.spacing.medium}>¿Que modalidad?</Text>
+                    <ScrollView showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={{ gap: 16 }}>
+                      {
+                        mockSportMode.map((sport, index) => (
+                          <TouchableWithoutFeedback onPress={() => setSelectedSportMode(sport._id)}>
+                            <MotiView
+                              style={{
+                                width: scale(88),
+                                height: scale(88),
+                                borderWidth: 1,
+                                borderRadius: 56,
+                                justifyContent: 'center',
+                                marginLeft: index === 0 ? 16 : 0,
+                                marginRight: index === mockSportMode.length - 1 ? 16 : 0,
+                              }}
+                              animate={{
+                                backgroundColor: selectedSportMode === sport._id ? 'black' : 'white',
+                              }}
+                              transition={{
+                                type: 'timing',
+                                duration: 300,
+                              }}
+                            >
+                              <MotiText
+                                style={{
+                                  textAlign: 'center',
+                                  lineHeight: verticalScale(88),
+                                  fontSize: customTheme.fontSize.xl,
+                                }}
+                                animate={{
+                                  color: interpolateColor(
+                                    selectedSportMode === sport._id ? 1 : 0, // Esto controla la interpolación
+                                    [0, 1], // El rango de la animación
+                                    ['black', 'white'] // Los colores entre los que interpolará
+                                  ),
+                                }}
+                                transition={{
+                                  type: 'timing',
+                                  duration: 300,
+                                }}
+                              >
+                                {sport.name}
+                              </MotiText>
+                            </MotiView>
+                            {/* <Div
+                            w={scale(88)}
+                            h={scale(88)}
+                            borderWidth={1}
+                            rounded={56}
+                            justifyContent='center'
+                            ml={index === 0 ? 16 : 0}
+                            mr={index === mockSportMode.length - 1 ? 16 : 0}
+                            bg={selectedSportMode === sport._id? "black" : "white"}
+                          >
+                            <Text color={selectedSportMode === sport._id? "white" : "black"} textAlign='center' lineHeight={verticalScale(88)} fontSize={customTheme.fontSize.xl}>{sport.name}</Text>
+                          </Div> */}
+                          </TouchableWithoutFeedback>
+                        ))
+                      }
+                    </ScrollView>
+                  </Div>
+                </>
+              }
+            </Div>
+          </MotiView>
+          {/* </TouchableWithoutFeedback> */}
           {/* <Div borderWidth={1} h={verticalScale(342)} p={customTheme.spacing.small} rounded={"lg"} alignItems='center'>
             <Text fontSize={customTheme.fontSize.small}>Deporte</Text>
           </Div> */}
@@ -354,7 +487,7 @@ const MatchModalHandler = ({ open, setOpen, match }: { open: boolean; setOpen: R
             />
           </Div> */}
 
-          <Div mb="lg">
+          {/* <Div mb="lg">
             <Text mb="sm">Fecha</Text>
             <Button onPress={() => setShowCalendar(true)}>Seleccionar Fecha</Button>
             {showCalendar &&
@@ -409,7 +542,7 @@ const MatchModalHandler = ({ open, setOpen, match }: { open: boolean; setOpen: R
               }}
             />
           </Div>
-          {/* Buttons */}
+         
           <Div row justifyContent="space-between" mt="lg">
             <Button bg="red600" color="white" onPress={() => setOpen(false)} rounded="lg">
               Cancel
@@ -417,7 +550,7 @@ const MatchModalHandler = ({ open, setOpen, match }: { open: boolean; setOpen: R
             <Button bg="blue600" color="white" onPress={match ? editMatch : createMatch} rounded="lg">
               Save
             </Button>
-          </Div>
+          </Div> */}
         </ScrollView>
       </Div>
       {/* </Overlay> */}
