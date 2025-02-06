a832b8d36865cfc06b00a0a8d7556c24c186e7f8
StalaDev <siminicnicolas@gmail.com>
2025-02-06 13:42:21 -0300

    need changes

--- mobile/components/modal/matchModalHandler.tsx
+++ mobile/components/modal/matchModalHandler.tsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Div, Overlay, Text, Input } from "react-native-magnus";
-import SelectDropdown from 'react-native-select-dropdown';
-import sportService from '../../service/sport.service';
-import useFetch from '../../hooks/useGet';
-import { QUERY_KEYS } from '../../types/query.types';
-import sportmodeService from '../../service/sportmode.service';
-import { StyleSheet } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import locationService from '../../service/location.service';
-import RNDateTimePicker from '@react-native-community/datetimepicker';
+import { Button, Div, Overlay, Text, Input, Modal } from "react-native-magnus";
+import { Keyboard, ScrollView, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
 import matchService from '../../service/match.service';
 import { useSession } from '../../context/authProvider';
 import { AppScreens } from '../../navigation/screens';
 import { navigate } from '../../utils/navigation';
 import Match from '../../types/match.type';
+import { customTheme } from '../../utils/theme';
+import { scale, verticalScale } from 'react-native-size-matters';
+import { MotiView } from 'moti';
 
 const mockHours = [
   "10:00",
@@ -33,56 +28,60 @@ const MatchModalHandler = ({ open, setOpen, match }: { open: boolean; setOpen: R
   const [showTimePicker, setShowTimePicker] = useState(false)
   const [showCalendar, setShowCalendar] = useState(false)
   const { currentUser } = useSession()
-  const fetchSportModesById = async () => {
-    if (selectedSport) {
-      if (!match) setSelectedSportMode('')
-      const res = await sportmodeService.getModesBySportId(selectedSport)
-      console.log('POR ID')
-      return res
-    } else {
-      const res = await sportmodeService.getAll()
-      console.log("TODO")
-      return res
-    }
-  }
 
-  const onChangeDate = (event, selectedDate) => {
-    setShowCalendar(false);
+  // NUEVO
+  const [openSecond, setOpenSecond] = useState(false)
 
-    if (event.type === "set" && selectedDate) {
-      setDate(selectedDate); 
-    }
-  };
+  // const fetchSportModesById = async () => {
+  //   if (selectedSport) {
+  //     if (!match) setSelectedSportMode('')
+  //     const res = await sportmodeService.getModesBySportId(selectedSport)
+  //     console.log('POR ID')
+  //     return res
+  //   } else {
+  //     const res = await sportmodeService.getAll()
+  //     console.log("TODO")
+  //     return res
+  //   }
+  // }
 
-  const onChangeTime = (event, selectedTime) => {
-    setShowTimePicker(false); 
+  // const onChangeDate = (event, selectedDate) => {
+  //   setShowCalendar(false);
 
-    if (event.type === "set" && selectedTime) {
-      setTime(selectedTime); 
-    }
-  };
+  //   if (event.type === "set" && selectedDate) {
+  //     setDate(selectedDate);
+  //   }
+  // };
+
+  // const onChangeTime = (event, selectedTime) => {
+  //   setShowTimePicker(false);
+
+  //   if (event.type === "set" && selectedTime) {
+  //     setTime(selectedTime);
+  //   }
+  // };
 
   // Get info
-  const { data: sports } = useFetch(sportService.getAll, [QUERY_KEYS.SPORTS]);
-  const { data: sportModes, isFetching } = useFetch(fetchSportModesById, [QUERY_KEYS.SPORT_MODES, selectedSport]);
-  const { data: locations } = useFetch(locationService.getAll, [QUERY_KEYS.LOCATIONS])
+  // const { data: sports } = useFetch(sportService.getAll, [QUERY_KEYS.SPORTS]);
+  // const { data: sportModes, isFetching } = useFetch(sportmodeService.getAll, [QUERY_KEYS.SPORT_MODES]);
+  // const { data: locations } = useFetch(locationService.getAll, [QUERY_KEYS.LOCATIONS])
 
-  useEffect(() => {
-    if (match) return
-    if (sports) {
-      setSelectedSport(sports.data[0]._id)
-    }
-  }, [sports])
+  // useEffect(() => {
+  //   if (match) return
+  //   if (sports) {
+  //     setSelectedSport(sports.data[0]._id)
+  //   }
+  // }, [sports])
 
-  useEffect(() => {
-    if (match) {
-      setSelectedSportMode(match.sportMode)
-      return
-    }
-    if (selectedSport && sportModes) {
-      setSelectedSportMode(sportModes.data[0]._id)
-    }
-  }, [sportModes])
+  // useEffect(() => {
+  //   if (match) {
+  //     setSelectedSportMode(match.sportMode)
+  //     return
+  //   }
+  //   if (selectedSport && sportModes) {
+  //     setSelectedSportMode(sportModes.data[0]._id)
+  //   }
+  // }, [sportModes])
 
   const createMatch = async () => {
     try {
@@ -102,7 +101,7 @@ const MatchModalHandler = ({ open, setOpen, match }: { open: boolean; setOpen: R
 
   const editMatch = async () => {
     if (!match) return
-    try{
+    try {
       const res = await matchService.update(match._id, {
         name: "BORRAR-5-con location",
         date: date,
@@ -113,270 +112,66 @@ const MatchModalHandler = ({ open, setOpen, match }: { open: boolean; setOpen: R
       })
     } catch (e) {
 
-    } finally{
+    } finally {
 
     }
   }
 
-  if (!sports || !sportModes) return null;
+  const mockSport = [{ name: 'Futbol', _id: '123' }, { name: 'Basquet', _id: '321' }, { name: "padel", id: '434' }]
+  const mockSportMode = [{ name: '5', _id: '123' }, { name: '7', _id: '321' }, { name: "11", id: '434' }]
+
+
+
+  // if (!sports || !sportModes) return null;
   return (
-    <Overlay visible={open} onBackdropPress={() => setOpen(false)} p="lg" rounded="lg">
-      <Div>
-        <Text fontSize="xl" mb="md" textAlign="center">
-          {match ? 'Editar partido' : 'Crear partido'}
-        </Text>
-        {
-          !match &&
-          <>
-            {/* Select 1 */}
-            <Div mb="lg">
-              <Text mb="sm">Select Option 1</Text>
-              <SelectDropdown
-                data={sports.data.map((item) => item.name)}
-                onSelect={(selectedItem, index) => {
-                  setSelectedSport(sports.data[index]._id);
-                }}
-                defaultValueByIndex={0}
-                defaultButtonText="Choose an option"
-                buttonTextAfterSelection={(selectedItem, index) => selectedItem}
-                rowTextForSelection={(item, index) => item}
-                buttonStyle={styles.dropdownButtonStyle}
-                buttonTextStyle={styles.dropdownButtonTxtStyle}
-                dropdownStyle={styles.dropdownMenuStyle}
-                rowTextStyle={styles.dropdownItemTxtStyle}
-                renderDropdownIcon={isOpened => (
-                  <Text style={styles.dropdownButtonArrowStyle}>
-                    {isOpened ? '▲' : '▼'}
-                  </Text>
-                )}
-                renderItem={(item, index, isSelected) => {
-                  return (
-                    <Div style={{ ...styles.dropdownItemStyle, ...(isSelected && { backgroundColor: '#D2D9DF' }) }}>
+    <Modal isVisible={open} onBackButtonPress={() => setOpen(false)}>
+      <Div p={customTheme.spacing.medium}>
+        <ScrollView keyboardShouldPersistTaps="handled" nestedScrollEnabled={true}>
+          <MotiView
+            from={{ height: verticalScale(50), borderWidth: 1 }}
+            animate={{ height: openSecond ? verticalScale(123) : verticalScale(35), borderWidth: 1 }}
+            transition={{ type: "spring", damping: 15 }}
+            style={{
+              borderColor: "#000",
+              borderRadius: 10,
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Div
+              flex={1}
+              w={"100%"}
+            >
+              {/* TITULO */}
+              {!openSecond &&
+                <TouchableWithoutFeedback onPress={() => setOpenSecond(true)}>
+                  <Div py={customTheme.spacing.small} h={verticalScale(35)} w={"100%"}>
+                    <Text>Cupos</Text>
+                  </Div>
+                </TouchableWithoutFeedback>
+              }
+              {
+                openSecond &&
+                <Div
+                  p={customTheme.spacing.medium}
+                  style={{ gap: 16 }}
+                  flex={1}
+                >
+                  <Text fontSize={customTheme.fontSize.medium} px={customTheme.spacing.medium}>Cupo</Text>
+                  <Div style={{gap:8}} flexDir='row' bg='red' flex={1} alignItems='center' >
+                    <Div w={scale(119)} h={verticalScale(56)} justifyContent='center' bg='green'><Text textAlign='center' fontSize={customTheme.fontSize.xl}>-</Text></Div>
+                    <Div w={scale(56)} h={verticalScale(56)} justifyContent='center'><Text fontSize={customTheme.fontSize.xl} textAlign='center'>10</Text></Div>
+                    <Div w={scale(119)} h={verticalScale(56)} justifyContent='center' bg='green'><Text textAlign='center' fontSize={customTheme.fontSize.xl}>+</Text></Div>
 
-                      <Text style={styles.dropdownItemTxtStyle}>{item}</Text>
-                    </Div>
-                  );
-                }}
-                renderButton={(selectedItem, isOpened) => {
-                  return (
-                    <Div style={styles.dropdownButtonStyle}>
-                      <Text style={styles.dropdownButtonTxtStyle}>
-                        {selectedItem ? selectedItem : 'Choose an option'}
-                      </Text>
-                      <Text style={styles.dropdownButtonArrowStyle}>
-                        {isOpened ? '▲' : '▼'}
-                      </Text>
-                    </Div>
-                  );
-                }}
-              />
+                  </Div>
+                </Div>
+              }
             </Div>
-          </>
-        }
-        {/* Select 2 */}
-        {
-          isFetching ?
-            <Text>Cargando</Text>
-            :
-            <Div mb="lg">
-              <Text mb="sm">Select Option 2</Text>
-              <SelectDropdown
-                data={sportModes.data.map((item) => item.name)}
-                onSelect={(selectedItem, index) => {
-                  setSelectedSportMode(sportModes.data[index]._id);
-                }}
-                defaultValueByIndex={match ? sportModes.data.findIndex(modes => modes._id === selectedSportMode) : 0}
-                defaultButtonText="Choose an option"
-                buttonTextAfterSelection={(selectedItem, index) => selectedItem}
-                rowTextForSelection={(item, index) => item}
-                buttonStyle={styles.dropdownButtonStyle}
-                buttonTextStyle={styles.dropdownButtonTxtStyle}
-                dropdownStyle={styles.dropdownMenuStyle}
-                rowTextStyle={styles.dropdownItemTxtStyle}
-                renderDropdownIcon={isOpened => (
-                  <Text style={styles.dropdownButtonArrowStyle}>
-                    {isOpened ? '▲' : '▼'}
-                  </Text>
-                )}
-                renderItem={(item, index, isSelected) => {
-                  return (
-                    <Div style={{ ...styles.dropdownItemStyle, ...(isSelected && { backgroundColor: '#D2D9DF' }) }}>
+          </MotiView>
 
-                      <Text style={styles.dropdownItemTxtStyle}>{item}</Text>
-                    </Div>
-                  );
-                }}
-                renderButton={(selectedItem, isOpened) => {
-                  return (
-                    <Div style={styles.dropdownButtonStyle}>
-                      <Text style={styles.dropdownButtonTxtStyle}>
-                        {selectedItem ? selectedItem : 'Choose an option'}
-                      </Text>
-                      <Text style={styles.dropdownButtonArrowStyle}>
-                        {isOpened ? '▲' : '▼'}
-                      </Text>
-                    </Div>
-                  );
-                }}
-              />
-            </Div>
-        }
-
-
-        {/* Select 3 */}
-        <Div mb="lg">
-          <Text mb="sm">Select Option 3</Text>
-          <SelectDropdown
-            data={['Privado', 'Publico']}
-            onSelect={(selectedItem, index) => {
-              setSelectValue3(selectedItem === 'Privado' ? 'private' : 'public');
-            }}
-            defaultValueByIndex={0}
-            defaultButtonText="Choose an option"
-            buttonTextAfterSelection={(selectedItem, index) => selectedItem}
-            rowTextForSelection={(item, index) => item}
-            buttonStyle={styles.dropdownButtonStyle}
-            buttonTextStyle={styles.dropdownButtonTxtStyle}
-            dropdownStyle={styles.dropdownMenuStyle}
-            rowTextStyle={styles.dropdownItemTxtStyle}
-            renderDropdownIcon={isOpened => (
-              <Text style={styles.dropdownButtonArrowStyle}>
-                {isOpened ? '▲' : '▼'}
-              </Text>
-            )}
-            renderItem={(item, index, isSelected) => {
-              return (
-                <Div style={{ ...styles.dropdownItemStyle, ...(isSelected && { backgroundColor: '#D2D9DF' }) }}>
-
-                  <Text style={styles.dropdownItemTxtStyle}>{item}</Text>
-                </Div>
-              );
-            }}
-            renderButton={(selectedItem, isOpened) => {
-              return (
-                <Div style={styles.dropdownButtonStyle}>
-                  <Text style={styles.dropdownButtonTxtStyle}>
-                    {selectedItem ? selectedItem : 'Choose an option'}
-                  </Text>
-                  <Text style={styles.dropdownButtonArrowStyle}>
-                    {isOpened ? '▲' : '▼'}
-                  </Text>
-                </Div>
-              );
-            }}
-          />
-        </Div>
-
-        {/* Inputs */}
-        <Div mb="lg">
-          <Text mb="sm">Select Location</Text>
-          <SelectDropdown
-            data={locations.data.map((item) => item.name)}
-            onSelect={(selectedItem, index) => {
-              setSelectedLocation(locations.data[index]._id);
-            }}
-            defaultValueByIndex={match ? locations.data.findIndex(location => location._id === selectedLocation) : null}
-            defaultButtonText="Choose an option"
-            buttonTextAfterSelection={(selectedItem, index) => selectedItem}
-            rowTextForSelection={(item, index) => item}
-            buttonStyle={styles.dropdownButtonStyle}
-            buttonTextStyle={styles.dropdownButtonTxtStyle}
-            dropdownStyle={styles.dropdownMenuStyle}
-            rowTextStyle={styles.dropdownItemTxtStyle}
-            renderDropdownIcon={isOpened => (
-              <Text style={styles.dropdownButtonArrowStyle}>
-                {isOpened ? '▲' : '▼'}
-              </Text>
-            )}
-            renderItem={(item, index, isSelected) => {
-              return (
-                <Div style={{ ...styles.dropdownItemStyle, ...(isSelected && { backgroundColor: '#D2D9DF' }) }}>
-
-                  <Text style={styles.dropdownItemTxtStyle}>{item}</Text>
-                </Div>
-              );
-            }}
-            renderButton={(selectedItem, isOpened) => {
-              return (
-                <Div style={styles.dropdownButtonStyle}>
-                  <Text style={styles.dropdownButtonTxtStyle}>
-                    {selectedItem ? selectedItem : 'Choose an option'}
-                  </Text>
-                  <Text style={styles.dropdownButtonArrowStyle}>
-                    {isOpened ? '▲' : '▼'}
-                  </Text>
-                </Div>
-              );
-            }}
-          />
-        </Div>
-
-        <Div mb="lg">
-          <Text mb="sm">Fecha</Text>
-          <Button onPress={() => setShowCalendar(true)}>Seleccionar Fecha</Button>
-          {showCalendar &&
-            <DateTimePicker
-              value={date}
-              mode="date" // Puedes cambiar a "time" o "datetime"
-              display="default" // "default", "spinner", "calendar", "clock"
-              onTouchCancel={() => setShowCalendar(false)}
-              onChange={onChangeDate}
-            />
-          }
-        </Div>
-
-        <Div mb="lg">
-          <Text mb="sm">Select hour</Text>
-          <SelectDropdown
-            data={mockHours}
-            onSelect={(selectedItem, index) => {
-              setTime(mockHours[index]);
-            }}
-            defaultButtonText="Choose an option"
-            buttonTextAfterSelection={(selectedItem, index) => selectedItem}
-            rowTextForSelection={(item, index) => item}
-            buttonStyle={styles.dropdownButtonStyle}
-            buttonTextStyle={styles.dropdownButtonTxtStyle}
-            dropdownStyle={styles.dropdownMenuStyle}
-            rowTextStyle={styles.dropdownItemTxtStyle}
-            renderDropdownIcon={isOpened => (
-              <Text style={styles.dropdownButtonArrowStyle}>
-                {isOpened ? '▲' : '▼'}
-              </Text>
-            )}
-            renderItem={(item, index, isSelected) => {
-              return (
-                <Div style={{ ...styles.dropdownItemStyle, ...(isSelected && { backgroundColor: '#D2D9DF' }) }}>
-
-                  <Text style={styles.dropdownItemTxtStyle}>{item}</Text>
-                </Div>
-              );
-            }}
-            renderButton={(selectedItem, isOpened) => {
-              return (
-                <Div style={styles.dropdownButtonStyle}>
-                  <Text style={styles.dropdownButtonTxtStyle}>
-                    {selectedItem ? selectedItem : 'Choose an option'}
-                  </Text>
-                  <Text style={styles.dropdownButtonArrowStyle}>
-                    {isOpened ? '▲' : '▼'}
-                  </Text>
-                </Div>
-              );
-            }}
-          />
-        </Div>
-        {/* Buttons */}
-        <Div row justifyContent="space-between" mt="lg">
-          <Button bg="red600" color="white" onPress={() => setOpen(false)} rounded="lg">
-            Cancel
-          </Button>
-          <Button bg="blue600" color="white" onPress={match ? editMatch : createMatch} rounded="lg">
-            Save
-          </Button>
-        </Div>
+        </ScrollView>
       </Div>
-    </Overlay>
+    </Modal>
   );
 };
 
