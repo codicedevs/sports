e11659fcf211b76ab6cbf7a8de4ca86a17394a2c
falgarania <algarania.franco@gmail.com>
2025-02-07 16:35:16 -0300

    hecho el componenete del torneo

--- mobile/screens/home.tsx
+++ mobile/screens/home.tsx
@@ -3,20 +3,22 @@ import { AppScreenProps, AppScreens } from "../navigation/screens";
 import { Button, Div, Text } from "react-native-magnus";
 import MatchInvitation from "../components/invitationCard";
 import TournamentCard from "../components/tournamentCard";
+import { customTheme } from "../utils/theme";
+import { scale } from "react-native-size-matters";
 
 const HomeScreen: React.FC<AppScreenProps<AppScreens.HOME_SCREEN>> = ({
   navigation,
 }) => {
   return (
-    <Div> /{/*carta superior de invitacion*/}
-      <Div style={{ padding: 10, marginTop: 30 }}>
+    <Div> /{/*carta superior de invitacion =)*/}
+      <Div style={{ padding: customTheme.spacing.small, marginTop: scale(26) }}>
         <MatchInvitation
           title="Ramiro te ha invitado a un partido"
           matchType="Futbol 5"
           date="Vi 25/01"
           time="19:00hs"
         />
-      </Div> {/* carta del trneo */}
+      </Div> {/* carta del trneo =$*/}
       <TournamentCard
         title="TORNEO VERANO FUTBOL ONCE"
         date="21/02"
