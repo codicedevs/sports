48a7a152b3a2c7f682e04b3b97d9fac1865e76bd
StalaDev <siminicnicolas@gmail.com>
2025-02-07 15:03:29 -0300

    changes

--- mobile/components/modal/matchModalHandler.tsx
+++ mobile/components/modal/matchModalHandler.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { ScrollView, TouchableWithoutFeedback, View } from 'react-native';
 import { Div, Modal, Text } from "react-native-magnus";
 import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
-import SportButton from '../matche/Form/sportButton';
-import SportModeButton from '../matche/Form/sportModeButton';
 import { customTheme } from '../../utils/theme';
+import SportInput from '../matche/Inputs/sport';
+import { Accordion } from '../collapsibleView';
 
 interface ModalProps {
   open: boolean,
@@ -12,51 +12,7 @@ interface ModalProps {
 }
 
 const MatchModalHandler = ({ open, setOpen }: ModalProps) => {
-  // Estados de selección
-  const [selectedSport, setSelectedSport] = useState('');
-  const [selectedSportMode, setSelectedSportMode] = useState('');
-
-  // Mock de deportes y modalidades
-  const mockSport = [{ name: 'Futbol', _id: '123' }, { name: 'Basquet', _id: '321' }, { name: "Padel", _id: '434' }];
-  const mockSportMode = [{ name: '5', _id: '123' }, { name: '7', _id: '321' }, { name: "11", _id: '434' }];
-
-  // Estados de animación para cada deporte y modalidad
-  const sportAnimations = mockSport.map(() => useSharedValue(0));
-  const modeAnimations = mockSportMode.map(() => useSharedValue(0));
-
-  // Función para manejar selección de deporte con fade
-  const handleSelectSport = (sportId: string, index: number) => {
-    setSelectedSport(sportId);
-
-    sportAnimations.forEach((animation, i) => {
-      animation.value = withTiming(i === index ? 1 : 0, { duration: 80 });
-    });
-  };
-
-  // Función para manejar selección de modalidad con fade
-  const handleSelectMode = (modeId: string, index: number) => {
-    setSelectedSportMode(modeId);
-
-    modeAnimations.forEach((animation, i) => {
-      animation.value = withTiming(i === index ? 1 : 0, { duration: 80 });
-    });
-  };
-
-  //DIV EXPANDIDO
-
-  const height = useSharedValue(50); // Comienza en 50px
-  const [expanded, setExpanded] = useState(false); // Estado de expansión
-
-  // Estilos animados
-  const animatedStyle = useAnimatedStyle(() => ({
-    height: height.value, // Se anima dinámicamente
-  }));
-
-  // Función para manejar el press y cambiar la altura
-  const toggleHeight = () => {
-    setExpanded(!expanded);
-    height.value = withTiming(expanded ? 50 : 342, { duration: 500 }); // Alterna altura
-  };
+  const [openId, setOpenId] = useState(null);
 
   return (
     <Modal isVisible={open} onBackButtonPress={() => setOpen(false)}>
@@ -64,68 +20,9 @@ const MatchModalHandler = ({ open, setOpen }: ModalProps) => {
         {
           <ScrollView keyboardShouldPersistTaps="handled" nestedScrollEnabled={true}>
             <Div w={"100%"} >
-              <Div>
-                <Animated.View
-                  style={[
-                    {
-                      width: '100%',
-                      borderWidth: 1,
-                      borderColor: 'black',
-                      height: "100%",
-                      paddingVertical: customTheme.spacing.small,
-                      borderRadius: customTheme.borderRadius.medium
-                    },
-                    animatedStyle,
-                  ]}
-                >
-                  {
-                    !expanded ?
-                      <TouchableWithoutFeedback onPress={toggleHeight}>
-                        <Div justifyContent='center' h={"100%"} px={customTheme.spacing.medium}>
-                          <Text fontSize={customTheme.fontSize.medium}  fontFamily='NotoSans-Variable'>Deportes</Text>
-                        </Div>
-                      </TouchableWithoutFeedback>
-                      :
-                      <>
-                        <Div pb={customTheme.spacing.medium} style={{ gap: 8 }}>
-                          <Text fontSize={16} px={customTheme.spacing.medium}>¿Qué deporte juegan?</Text>
-                          <ScrollView showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={{ gap: 16 }}>
-                            {mockSport.map((sport, index) => (
-                              <SportButton
-                                key={sport._id}
-                                sport={sport}
-                                index={index}
-                                onPress={handleSelectSport}
-                                animationValue={sportAnimations[index]}
-                                length={mockSport.length}
-                              />
-                            ))}
-                          </ScrollView>
-                        </Div>
-
-                        <Div px={16}>
-                          <View style={{ width: '100%', borderBottomWidth: 1, borderStyle: "dotted" }} />
-                        </Div>
-
-                        <Div pt={customTheme.spacing.medium} style={{ gap: 8 }}>
-                          <Text fontSize={16} px={16}>¿Qué modalidad?</Text>
-                          <ScrollView showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={{ gap: 16 }}>
-                            {mockSportMode.map((mode, index) => (
-                              <SportModeButton
-                                key={mode._id}
-                                mode={mode}
-                                index={index}
-                                onPress={handleSelectMode}
-                                animationValue={modeAnimations[index]}
-                                length={mockSportMode.length}
-                              />
-                            ))}
-                          </ScrollView>
-                        </Div>
-                      </>
-                  }
-                </Animated.View>
-              </Div>
+              <Accordion id='sports' title={"Deportes"} openId={openId} setOpenId={setOpenId} >
+                <SportInput />
+              </Accordion>
             </Div>
           </ScrollView>
         }
