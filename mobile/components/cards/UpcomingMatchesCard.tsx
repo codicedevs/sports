24397128e3d2e49b3273f104fad3ca0d7a368cd1
manuvant <67987587+manuvant@users.noreply.github.com>
2025-02-10 15:13:23 -0300

    finish component

--- mobile/components/cards/UpcomingMatchesCard.tsx
+++ mobile/components/cards/UpcomingMatchesCard.tsx
@@ -1,5 +1,5 @@
 import { Div, Image, Text } from "react-native-magnus";
-import { scale } from "react-native-size-matters";
+import { scale, verticalScale } from "react-native-size-matters";
 import { customTheme } from "../../utils/theme";
 import reloj from "@assets/reloj.png";
 import players from "@assets/players.png";
@@ -9,43 +9,35 @@ import flecha from "@assets/flecha.png";
 const UpcomingMatchCard = () => {
     return (
         <Div
-            h={"auto"}
+            h={verticalScale(160)}
             w={scale(150)}
             rounded="md"
             borderWidth={1}
             borderColor="black"
-            pt={customTheme.spacing.small}
-            pb={customTheme.spacing.small}
-            alignItems="center"
+            p={customTheme.spacing.medium}
+            justifyContent="space-between"
         >
-            <Div flexDir="row" alignItems="center" h={"auto"} mt={scale(5)}>
-                <Image source={reloj} h={scale(17)} w={scale(17)} mr={5} resizeMode="contain" mb={scale(3)} />
-                <Text w={128} h={"auto"} fontSize={17}>
+            <Div flexDir="row">
+                <Image source={reloj} h={customTheme.spacing.medium} w={scale(15)} mt={scale(2)} mr={customTheme.spacing.xs} resizeMode="contain" />
+                <Text fontSize={customTheme.fontSize.medium} fontFamily="NotoSans-Variable">
                     Vi 30/12 00:00
                 </Text>
             </Div>
 
             <Text
                 fontSize={20}
-                p={10}
-                mt={scale(5)}
                 fontFamily="NotoSans-BoldItalic"
             >
                 SUPER CLUB FUTBOL 5</Text>
             <Div
-                w={128}
-                h={"auto"}
                 flexDir="row"
-                alignItems="center"
                 justifyContent="space-between"
-                mt={scale(10)}
-                mb={scale(6)}
             >
-                <Div bg="#D9FA53" flexDir="row" h={"auto"} >
-                    <Image source={players} h={20} w={20} bg="#D9FA53" resizeMode="contain" />
+                <Div bg="#D9FA53" flexDir="row" >
+                    <Image source={players} bg="#D9FA53" resizeMode="contain"  w={customTheme.fontSize.medium}/>
                     <Text ml={5} fontSize={customTheme.fontSize.medium} >6/10</Text>
                 </Div>
-                <Image source={flecha} h={22} w={22} resizeMode="contain" position="absolute" right={scale(-8)} />
+                <Image source={flecha}  resizeMode="contain" w={customTheme.fontSize.title} />
             </Div>
         </Div>
     )
