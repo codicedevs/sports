aa5c5a276d031e837cd2dcb51c9b09490d9f3937
StalaDev <siminicnicolas@gmail.com>
2025-02-10 13:05:06 -0300

    upgrade

--- mobile/components/matche/Inputs/matchPrivacyToggle.tsx
+++ mobile/components/matche/Inputs/matchPrivacyToggle.tsx
@@ -2,29 +2,52 @@ import React, { useState } from 'react'
 import { Div, Text } from 'react-native-magnus'
 import { customTheme } from '../../../utils/theme'
 import { verticalScale } from 'react-native-size-matters'
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native'
 
-type state = "public" | "private";
+type PrivacyOption = 'public' | 'private';
 
 const MatchPrivacyToggleInput = () => {
-    const [isOpen, setIsOpen] = useState<state>("private")
+    const [privacyOption, setPrivacyOption] = useState<PrivacyOption>('private');
+
+    const isPrivate = privacyOption === 'private';
+    const isPublic = privacyOption === 'public';
+
     return (
         <Div p={customTheme.spacing.medium} style={{ gap: 8 }}>
             <Div h={verticalScale(40)} flexDir='row'>
-                <Div flex={1} h={verticalScale(40)} justifyContent='center' borderWidth={1} bg={isOpen === "public" ? "white" : customTheme.colors.secondaryBackground}>
-                    <TouchableOpacity onPress={() => setIsOpen("private")}>
-                        <Text color={isOpen === "public" ? "black" : "white"} textAlign='center'>Privado</Text>
+                <Div
+                    flex={1}
+                    h={verticalScale(40)}
+                    justifyContent='center'
+                    borderWidth={1}
+                    bg={isPrivate ? customTheme.colors.secondaryBackground : 'white'}
+                >
+                    <TouchableOpacity onPress={() => setPrivacyOption('private')}>
+                        <Text color={isPrivate ? 'white' : 'black'} textAlign='center'>
+                            Privado
+                        </Text>
                     </TouchableOpacity>
                 </Div>
-                <Div flex={1} h={verticalScale(40)} justifyContent='center' borderWidth={1} bg={isOpen === "private" ? "white" : customTheme.colors.secondaryBackground}>
-                    <TouchableOpacity onPress={() => setIsOpen("public")}>
-                        <Text textAlign='center' color={isOpen === "private" ? "black" : "white"}>Publico</Text>
+                <Div
+                    flex={1}
+                    h={verticalScale(40)}
+                    justifyContent='center'
+                    borderWidth={1}
+                    bg={isPublic ? customTheme.colors.secondaryBackground : 'white'}
+                >
+                    <TouchableOpacity onPress={() => setPrivacyOption('public')}>
+                        <Text color={isPublic ? 'white' : 'black'} textAlign='center'>
+                            Público
+                        </Text>
                     </TouchableOpacity>
                 </Div>
             </Div>
-            <Text fontFamily='NotoSans-Variable' textAlign='auto'>Lorem ipsum odor amet, consectetuer adipiscing elit. Consequat vestibulum felis dis dis consectetur. Ornare etiam class ligula.</Text>
+            <Text fontFamily='NotoSans-Variable' textAlign='auto'>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Consequat vestibulum felis dis dis
+                consectetur. Ornare etiam class ligula.
+            </Text>
         </Div>
-    )
-}
+    );
+};
 
-export default MatchPrivacyToggleInput
+export default MatchPrivacyToggleInput;
