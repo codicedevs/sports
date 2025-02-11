4233355007132f632863b9c4d6fdf71946211649
falgarania <algarania.franco@gmail.com>
2025-02-11 14:03:28 -0300

    hecha la home
    tener en cta borrar el 1er scroll y luego agregar funcionalidades

--- mobile/screens/home.tsx
+++ mobile/screens/home.tsx
@@ -6,16 +6,18 @@ import { scale } from "react-native-size-matters";
 import { customTheme } from "../utils/theme";
 import TournamentCard from "../components/tournamentCard";
 import MatchCard from "../components/matchesCards";
-import UpcomingMatchCard from "../components/cards/UpcomingMatchesCard";
+import UpcomingMatchCard from "../components/UpcomingMatchesCard";
+import { ScrollView } from "react-native";
 
 const HomeScreen: React.FC<AppScreenProps<AppScreens.HOME_SCREEN>> = ({
   navigation,
 }) => {
   return (
     <Div p={customTheme.spacing.small}>
+      <ScrollView showsVerticalScrollIndicator={false}> {/* eliminar este scrool solo lo hice para mostrar todo */}
       {" "}
       /{/*carta superior de invitacion =)*/}
-      <Div style={{  marginTop: scale(26) }}>
+      <Div style={{ marginTop: scale(26) }}>
         <MatchInvitation
           title="Ramiro te ha invitado a un partido"
           matchType="Futbol 5"
@@ -29,8 +31,15 @@ const HomeScreen: React.FC<AppScreenProps<AppScreens.HOME_SCREEN>> = ({
         date="21/02"
         imageSource={require("../assets/fotoCardTorneo.png")}
       />
+      <ScrollView horizontal contentContainerStyle={{marginTop: 15}}>
+        {/* card upcoming */}
+        <UpcomingMatchCard fecha="Vi 30/12 " cupo="1/10" titulo="Super Club" />
+        <UpcomingMatchCard fecha="Mar 31/12 " cupo="3/22" titulo="La villa Club" />
+        <UpcomingMatchCard fecha="Lun 1/01 " cupo="9/10" titulo="Loyal" />
+      </ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
       {/* carta partidos */}
-     <MatchCard
+      <MatchCard
         day="Miércoles"
         date="9"
         time="22:00 hs"
@@ -38,6 +47,14 @@ const HomeScreen: React.FC<AppScreenProps<AppScreens.HOME_SCREEN>> = ({
         players={5}
         maxPlayers={10}
       />
+        <MatchCard
+        day="jueve"
+        date="12"
+        time="52:00 hs"
+        location="anda a sabe"
+        players={5}
+        maxPlayers={10}
+      /></ScrollView></ScrollView>
     </Div>
   );
 };
