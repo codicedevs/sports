ad7abe8c101b1061ca590c288d183f6dd0b37c2d
StalaDev <siminicnicolas@gmail.com>
2025-02-14 10:37:37 -0300

    added types

--- mobile/types/form.type.ts
+++ mobile/types/form.type.ts
@@ -8,3 +8,27 @@ export interface SportMode {
     _id: string;
     label: string
 }
+
+export type PrivacyOption = 'public' | 'private';
+
+interface Location {
+    type: "Point";
+    coordinates: [number, number];
+}
+
+export interface Place {
+    _id: string;
+    name: string;
+    address: string;
+    location: Location;
+    matches: any[];
+}
+
+export interface MatchDetails {
+    selectedSport: Sport | null;
+    selectedSportMode: SportMode | null;
+    playerLimit: number;
+    privacyOption: PrivacyOption;
+    matchDate: Date | null;
+    location: Place | null;
+}
