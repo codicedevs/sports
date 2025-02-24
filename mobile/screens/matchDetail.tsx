a498fba8bc6f8dea791929aaca84b85449a51a9e
falgarania <algarania.franco@gmail.com>
2025-02-24 14:29:55 -0300

    aaaa

--- mobile/screens/matchDetail.tsx
+++ mobile/screens/matchDetail.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppScreenProps, AppScreens } from "../navigation/screens";
 import { Text } from "react-native";
-import { Div } from "react-native-magnus";
+import { Div, Image } from "react-native-magnus";
 import matchService from "../service/match.service";
 import useFetch from "../hooks/useGet";
 import { QUERY_KEYS } from "../types/query.types";
@@ -10,8 +10,6 @@ const MatchDetail: React.FC<AppScreenProps<AppScreens.MATCH_DETAIL>> = ({
   route,
 }) => {
   const { id } = route.params;
-  console.log("Route params:", route.params); // <-- para ver si llega id
-  
 
   const fetchMatchInfo = async () => {
     const res = await matchService.getById(id);
@@ -25,6 +23,8 @@ const MatchDetail: React.FC<AppScreenProps<AppScreens.MATCH_DETAIL>> = ({
   return (
     <Div>
       <Text>Partido id: {id}</Text>
+      <Image source={require("../assets/iconflecha2.png")} />
+
     </Div>
   );
 };
