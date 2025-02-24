a604019bdd3da22d5bbb8c17165b976abc5e890f
CatoYuvone <otacyuvo@gmail.com>
2025-02-24 12:53:06 -0300

    [Backend] Corrected zone.dto.ts

--- api/src/zones/zone.dto.ts
+++ api/src/zones/zone.dto.ts
@@ -1,39 +1,23 @@
-import { IsString, IsNotEmpty, IsArray, ValidateNested, IsNumber } from 'class-validator';
+import { IsString, IsArray, ValidateNested, IsIn } from 'class-validator';
 import { Type } from 'class-transformer';
 import { PartialType } from '@nestjs/swagger';
 
-class CoordinateDto {
-  // @IsNumber({}, { each: true })
-  // @IsArray()
-  coordinates: number[]; // Un array de dos números [lng, lat]
+
+class ZoneLocationDto {
+  @IsIn(["Polygon"])
+  type: "Polygon";
+
+  @IsArray()
+  coordinates: [number, number][][];
 }
-
-class RingDto {
-  // @ValidateNested({ each: true })
-  @Type(() => CoordinateDto)
-  // @IsArray()
-  ring: CoordinateDto[]; // Un array de coordenadas
-}
-
-class LocationDto {
-  // @IsString()
-  // @IsNotEmpty()
-  type: string; // Siempre será "Polygon"
-
-  // @ValidateNested({ each: true })
-  @Type(() => RingDto)
-  // @IsArray()
-  coordinates: RingDto[]; // Array de anillos
-}
-
 export class CreateZoneDto {
-  // @IsString()
-  // @IsNotEmpty()
-  name: string; // Nombre de la zona
+  @IsString()
+  name: string;
 
-  // @ValidateNested()
-  @Type(() => LocationDto)
-  location: LocationDto; // Objeto para representar la ubicación (GeoJSON)
+  @ValidateNested()
+  @Type(() => ZoneLocationDto)
+  location: ZoneLocationDto;
 }
 
-export class UpdateZoneDto extends PartialType(CreateZoneDto) {}
+
+export class UpdateZoneDto extends PartialType(CreateZoneDto) { }
