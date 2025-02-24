3ddca555026166f151ae3c91bcbf852229b7b4a3
falgarania <algarania.franco@gmail.com>
2025-02-24 15:06:11 -0300

    aaaaa

--- mobile/screens/matchDetail.tsx
+++ mobile/screens/matchDetail.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { AppScreenProps, AppScreens } from "../navigation/screens";
-import { Text } from "react-native";
-import { Div, Image } from "react-native-magnus";
+
+import { Div, Image, Text } from "react-native-magnus";
 import matchService from "../service/match.service";
 import useFetch from "../hooks/useGet";
 import { QUERY_KEYS } from "../types/query.types";
+import { customTheme } from "../utils/theme";
 
 const MatchDetail: React.FC<AppScreenProps<AppScreens.MATCH_DETAIL>> = ({
   route,
@@ -21,10 +22,36 @@ const MatchDetail: React.FC<AppScreenProps<AppScreens.MATCH_DETAIL>> = ({
   ]);
 
   return (
-    <Div>
+    <Div p={customTheme.spacing.medium}>
       <Text>Partido id: {id}</Text>
-      <Image source={require("../assets/iconflecha2.png")} />
-
+      <Div flexDir="row" alignItems="center">
+        <Div justifyContent="flex-start">
+          <Image
+            source={require("../assets/iconflecha2.png")}
+            style={{ width: 20, height: 20, resizeMode: "contain" }}
+          />
+        </Div>
+        <Div w="75%" alignItems="center">
+          <Text
+            fontSize={customTheme.fontSize.medium}
+            fontFamily="NotoSans-Variable"
+          >
+            Detalle del partido
+          </Text>
+        </Div>
+      </Div>
+      <Div flexDir="row" justifyContent="space-between">
+        <Div>
+          <Text>FUTBOL 5</Text>
+        </Div>
+        <Div flexDir="row">
+          <Image
+            source={require("../assets/iconUser.png")}
+            style={{ width: 20, height: 20, resizeMode: "contain" }}
+          />
+          <Text>5/10</Text>
+        </Div>
+      </Div>
     </Div>
   );
 };
