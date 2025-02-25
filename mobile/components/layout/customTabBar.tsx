7de13fc5c612b63be59c9f4b3473604d7d01251a
StalaDev <siminicnicolas@gmail.com>
2025-02-25 13:25:53 -0300

    need change of colors

--- mobile/components/layout/customTabBar.tsx
+++ mobile/components/layout/customTabBar.tsx
@@ -5,16 +5,22 @@ import { Icon, Div, Button } from "react-native-magnus";
 import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 import { AppScreens } from "../../navigation/screens";
 import { customTheme } from "../../utils/theme";
+import HomeIcon from "@assets/tabIcons/Icons/Home";
+import { scale } from "react-native-size-matters";
+import BellIcon from "@assets/tabIcons/Icons/Bell";
+import PlusIcon from "@assets/tabIcons/Icons/Plus";
+import FieldIcon from "@assets/tabIcons/Icons/Field";
+import ProfileIcon from "@assets/tabIcons/Icons/Profile";
 
 const CustomTabBar: React.FC<BottomTabBarProps> = ({ state, descriptors, navigation }) => {
   const insets = useSafeAreaInsets();
 
   const iconMap: Record<string, JSX.Element> = {
-    HomeStack: <Icon fontFamily="Octicons" name="home" fontSize={customTheme.fontSize.xl} color="#aaa" />,
-    SettingsStack: <Icon fontFamily="Feather" name="bell" fontSize={customTheme.fontSize.xl} color="#aaa" />,
-    SettingsStack1: <Icon fontFamily="Feather" name="plus" fontSize={customTheme.fontSize.xl} color="#aaa" />,
-    [AppScreens.MATCH_SCREEN]: <Icon fontFamily="MaterialCommunityIcons" name="soccer-field" fontSize={customTheme.fontSize.xl} color="#aaa"   style={{ transform: [{ rotate: "90deg" }] }} />,
-    SettingsStack3: <Icon fontFamily="Feather" name="user" fontSize={customTheme.fontSize.xl} color="#aaa" />,
+    HomeStack: <HomeIcon width={scale(30)} height={scale(30)} />,
+    SettingsStack: <BellIcon width={scale(30)} height={scale(30)} />,
+    SettingsStack1: <PlusIcon width={scale(30)} height={scale(30)} />,
+    [AppScreens.MATCH_SCREEN]: <FieldIcon width={scale(30)} height={scale(30)} />,
+    SettingsStack3: <ProfileIcon width={scale(30)} height={scale(30)} />
   };
 
   const screen = getFocusedRouteNameFromRoute(state.routes[state.index]);
