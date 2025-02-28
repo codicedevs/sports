2ced671265d23e44cedfb4589eaa57b16e04ef47
StalaDev <siminicnicolas@gmail.com>
2025-02-28 13:49:44 -0300

    changes to recieve data

--- mobile/components/matche/Inputs/selectZone.tsx
+++ mobile/components/matche/Inputs/selectZone.tsx
@@ -17,25 +17,41 @@ const SelectZoneInput = ({ matchDetailsRef }: SportInputProps) => {
 
     const { data: zonas } = useFetch(zonesService.getZones, [QUERY_KEYS.ZONES])
     useEffect(() => {
-        matchDetailsRef.current.preferredZones = selectedZone;
-    }, [selectedZone, matchDetailsRef]);
+        if (zonas && matchDetailsRef.current.preferredZones) {
+            const updatedZone = (matchDetailsRef.current.preferredZones as (string | Zone)[]).map(item =>
+                typeof item === 'string'
+                    ? zonas.data.results.find((s: Zone) => s._id === item)
+                    : item
+            ).filter(Boolean) as Zone[];
+            setSelectedZone(updatedZone);
+            console.log(updatedZone)
+            matchDetailsRef.current.preferredZones = updatedZone;
+        }
+    }, [zonas]);
 
     const toggleZoneSelection = (zone: Zone) => {
-        const isSelected = selectedZone.some(z => z._id === zone._id)
-        if (isSelected) {
-            setSelectedZone(selectedZone.filter(z => z._id !== zone._id))
-        } else {
-            setSelectedZone([...selectedZone, zone])
-        }
-    }
-
-    const toggleSelectAll = () => {
+        setSelectedZone((prevSelected) => {
+          let updated: Zone[];
+          if (prevSelected.some((z) => z._id === zone._id)) {
+            updated = prevSelected.filter((z) => z._id !== zone._id);
+          } else {
+            updated = [...prevSelected, zone];
+          }
+          matchDetailsRef.current.preferredZones = updated;
+          return updated;
+        });
+      };
+      
+      const toggleSelectAll = () => {
+        let newSelected: Zone[];
         if (selectedZone.length === zonas.data.results.length) {
-            setSelectedZone([]);
+          newSelected = [];
         } else {
-            setSelectedZone(zonas.data.results);
+          newSelected = zonas.data.results;
         }
-    };
+        setSelectedZone(newSelected);
+        matchDetailsRef.current.preferredZones = newSelected;
+      };
 
     if (!zonas) return null
     return (
