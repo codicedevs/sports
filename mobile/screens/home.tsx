be5355880d4f5347aeaaa88a929be7e648b6ac0b
StalaDev <siminicnicolas@gmail.com>
2025-03-05 11:04:13 -0300

    using match

--- mobile/screens/home.tsx
+++ mobile/screens/home.tsx
@@ -4,6 +4,52 @@ import { Button, Div } from "react-native-magnus";
 import MatchModalHandler from "../components/modal/matchModalHandler";
 import { ModalContext } from "../context/modalProvider";
 import Field from "../components/field";
+import { useSession } from "../context/authProvider";
+
+const match = {
+  "_id": "67003e15c94369186bbdfb32",
+  "name": "Partido 1",
+  "playersLimit": 10,
+  "userId": {
+    "friends": [],
+    "roles": [
+      "user"
+    ],
+    "matches": [],
+    "groups": [],
+    "_id": "67c1f5c4810de319ef318ade"
+  },
+  "users": [
+    {
+      "friends": [],
+      "roles": [
+        "user"
+      ],
+      "matches": [],
+      "groups": [],
+      "_id": "67c1f5c4810de319ef318adf"
+    },
+    {
+      "friends": [],
+      "roles": [
+        "user"
+      ],
+      "matches": [],
+      "groups": [],
+      "_id": "67c1f5c4810de319ef318ae0"
+    },
+    {
+      "friends": [],
+      "roles": [
+        "user"
+      ],
+      "matches": [],
+      "groups": [],
+      "_id": "67c1f5c4810de319ef318ae1"
+    }
+  ],
+  "__v": 2
+}
 
 const HomeScreen: React.FC<AppScreenProps<AppScreens.HOME_SCREEN>> = ({
   navigation,
@@ -18,7 +64,7 @@ const HomeScreen: React.FC<AppScreenProps<AppScreens.HOME_SCREEN>> = ({
 
   return (
     <Div flex={1}>
-      <Field />
+      <Field match={match} />
     </Div>
   );
 };
