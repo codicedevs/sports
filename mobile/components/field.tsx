d37cd84841f6b7fa6d104c0c2a141b29b95b8c2c
StalaDev <siminicnicolas@gmail.com>
2025-03-06 15:22:40 -0300

    fixed modal

--- mobile/components/field.tsx
+++ mobile/components/field.tsx
@@ -1,11 +1,10 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import { Div, Input, Text } from "react-native-magnus";
 import { scale, verticalScale } from "react-native-size-matters";
 import Match from "../types/match.type";
-import {ScrollView, TouchableOpacity } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native";
 import { customTheme } from "../utils/theme";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import Modal from "react-native-modal";
 
 const personas = [
   "Santiago Rodríguez", "Valentina Gómez", "Mateo Fernández", "Lucía Pérez",
@@ -49,7 +48,7 @@ const TeamField = ({ playersCount, mirror = false, onPlayerPress, playersAssignm
   const maxPerRow = playersCount === 5 ? 2 : 4;
   const numRows = Math.ceil(fieldPlayers / maxPerRow);
   const [layout, setLayout] = useState(null);
-  
+
   const handleLayout = (event) => {
     const { width, height } = event.nativeEvent.layout;
     setLayout({ width, height });
@@ -105,7 +104,7 @@ const Field = ({ match }: FieldProps) => {
   const [open, setOpen] = useState(false);
   const [selectedPlayerId, setSelectedPlayerId] = useState(null);
   const [playersAssignments, setPlayersAssignments] = useState({});
-   const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const totalPlayers = match.playersLimit;
   const teamPlayers = totalPlayers / 2;
@@ -127,7 +126,7 @@ const Field = ({ match }: FieldProps) => {
     );
   }, [searchQuery]);
 
-  const iniciales = (playerId: string) =>{
+  const iniciales = (playerId: string) => {
     const GK = playersAssignments[playerId];
     const initials = GK ? GK.split(" ").map(word => word[0]).join("") : "+";
     return initials
@@ -135,69 +134,80 @@ const Field = ({ match }: FieldProps) => {
 
   return (
     <>
-     {open && (
-      <TouchableWithoutFeedback onPress={() => setOpen(false)} style={{height:"100%", backgroundColor:"rgba(0, 0, 0, 0.5)"}}>
-  <Div
-    position="absolute"
-    top={0}
-    left={0}
-    right={0}
-    bottom={0}
-    // bg="rgba(0, 0, 0, 0.5)"
-    alignItems="center"
-    justifyContent="center"
-    zIndex={1000}
-  >
-    <KeyboardAwareScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Div zIndex={2000} h={verticalScale(400)} bg="white" p={customTheme.spacing.medium} rounded="lg" shadow="md" w={scale(300)}>
-      <TouchableOpacity onPress={() => setOpen(false)} style={{paddingVertical:customTheme.spacing.small}}>
-        <Text>Close</Text>
-        </TouchableOpacity>
-        <Input
-          placeholder="Buscar..."
-          value={searchQuery}
-          onChangeText={(text) => setSearchQuery(text)}
-          mb={customTheme.spacing.medium}
-        />
-        <ScrollView style={{ maxHeight: verticalScale(300) }}>
-          {filteredPersonas.map((persona) => (
-            <TouchableOpacity key={persona} onPress={() => handlePersonaSelect(persona)}>
-              <Div p={customTheme.spacing.medium}>
-                <Text>{persona}</Text>
-              </Div>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      </Div>
-    </KeyboardAwareScrollView>
-  </Div>
-  </TouchableWithoutFeedback>
-)}
+      <Modal
+        isVisible={open}
+        animationIn="fadeIn"
+        animationInTiming={300}
+        animationOut="fadeOut"
+        animationOutTiming={300}
+        backdropOpacity={0.5}
+        onBackdropPress={() => setOpen(false)}
+        onBackButtonPress={() => setOpen(false)}
+        coverScreen={true}
+        style={{ margin: 0 }}
+      >
+
+        <Div
+          zIndex={2000}
+          h={verticalScale(400)}
+          bg="white"
+          p={customTheme.spacing.medium}
+          rounded="lg"
+          shadow="md"
+          w={scale(300)}
+          alignSelf="center"
+        >
+          <TouchableOpacity
+            onPress={() => setOpen(false)}
+            style={{ paddingVertical: customTheme.spacing.small }}
+          >
+            <Text>Close</Text>
+          </TouchableOpacity>
+          <Input
+            placeholder="Buscar..."
+            value={searchQuery}
+            onChangeText={(text) => setSearchQuery(text)}
+            mb={customTheme.spacing.medium}
+          />
+          <ScrollView style={{ maxHeight: verticalScale(300) }}>
+            {filteredPersonas.map((persona) => (
+              <TouchableOpacity
+                key={persona}
+                onPress={() => handlePersonaSelect(persona)}
+              >
+                <Div p={customTheme.spacing.medium}>
+                  <Text>{persona}</Text>
+                </Div>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </Div>
+      </Modal>
 
       <Div p={20} bg="green" h="100%">
         <Div id="first-half" borderWidth={1} flex={1}>
           <Div h="15%" alignItems="center" justifyContent="center">
-            <TouchableOpacity id="Top-GK" onPress={() =>handlePlayerPress("Top-GK")}>
-            <FPlayer size={scale(50)} initials={iniciales('Top-GK')} />
+            <TouchableOpacity id="Top-GK" onPress={() => handlePlayerPress("Top-GK")}>
+              <FPlayer size={scale(50)} initials={iniciales('Top-GK')} />
             </TouchableOpacity>
           </Div>
-          <TeamField 
-            playersCount={teamPlayers} 
-            mirror={false} 
-            onPlayerPress={handlePlayerPress} 
-            playersAssignments={playersAssignments} 
+          <TeamField
+            playersCount={teamPlayers}
+            mirror={false}
+            onPlayerPress={handlePlayerPress}
+            playersAssignments={playersAssignments}
           />
         </Div>
         <Div id="second-half" borderWidth={1} flex={1}>
-          <TeamField 
-            playersCount={teamPlayers} 
-            mirror={true} 
-            onPlayerPress={handlePlayerPress} 
-            playersAssignments={playersAssignments} 
+          <TeamField
+            playersCount={teamPlayers}
+            mirror={true}
+            onPlayerPress={handlePlayerPress}
+            playersAssignments={playersAssignments}
           />
           <Div h="15%" alignItems="center" justifyContent="center">
-          <TouchableOpacity id="Bottom-GK" onPress={() =>handlePlayerPress("Top-GK")}>
-            <FPlayer size={scale(50)} initials={iniciales('Bottom-GK')} />
+            <TouchableOpacity id="Bottom-GK" onPress={() => handlePlayerPress("Top-GK")}>
+              <FPlayer size={scale(50)} initials={iniciales('Bottom-GK')} />
             </TouchableOpacity>
           </Div>
         </Div>
