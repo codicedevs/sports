d41fe5b8478b75429446523bcb9bf524543261c4
StalaDev <siminicnicolas@gmail.com>
2025-03-10 13:58:38 -0300

    changes in match service

--- mobile/service/match.service.ts
+++ mobile/service/match.service.ts
@@ -6,7 +6,42 @@ class MatchService extends CRUDService<Match> {
   constructor() {
     super("matches");
   }
-  
+
+  getAll = async () => {
+    const res = await this.get(`/`);
+    return res.data;
+  };
+
+
+  update = async (id: string, matchInfo: CreateMatchDto) => {
+    const res = await this.put(`/${id}`, matchInfo)
+    return res.data
+  }
+
+  getById = async (id: string) => {
+    return await this.get(`/${id}`)
+  }
+
+  create = async (match: CreateMatchDto) => {
+    const res = await this.post(`/`, match);
+    return res
+  };
+
+  addPlayerToFormation = async (
+    matchId: string,
+    userId: string,
+    body: { team: number; position: number }
+  ) => {
+    await this.patch(`/${matchId}/formation/${userId}/add`, body)
+  }
+
+  removePlayerFromFormation = async (
+    matchId: string,
+    userId: string
+  ) => {
+    await this.patch(`/${matchId}/formation/${userId}/add`)
+  }
+
   acceptCreatematch = async (matchId: string) => {
     return this.put(`/accept/${matchId}`);
   };
