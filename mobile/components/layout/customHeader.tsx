10561f002fb2d9b0d6586c836d95cf05fb41b17a
StalaDev <nicosiminic@gmail.com>
2025-03-20 15:58:39 -0300

    fixed styles

--- mobile/components/layout/customHeader.tsx
+++ mobile/components/layout/customHeader.tsx
@@ -1,13 +1,35 @@
-import { Image } from "react-native"
+import { Image, TouchableOpacity } from "react-native"
 import { Div, Text } from "react-native-magnus"
 import { scale } from "react-native-size-matters"
 import { customTheme } from "../../utils/theme"
+import { useNavigation } from "@react-navigation/native"
+import { AppScreens } from "../../navigation/screens" // Asegúrate de importar correctamente AppScreens
 
 export const CustomHeader = () => {
+    const navigation = useNavigation();
+
+    const handleBackPress = () => {
+        if (navigation.canGoBack()) {
+            navigation.goBack();
+        } else {
+            navigation.reset({
+                index: 0,
+                routes: [{ name: "HomeStack", state: { routes: [{ name: AppScreens.HOME_SCREEN }] } }],
+            });
+        }
+    };
+
     return (
         <Div flexDir="row" alignItems="center" p={customTheme.spacing.medium} justifyContent="space-between">
-            <Image style={{ height: scale(28), width: scale(23), padding:customTheme.spacing.small }} source={require("../../assets/match/backIcon.png")} />
-            <Image style={{ height: scale(35), width: scale(35) }} source={require("../../assets/match/logoF.png")} />
+            <Div flexDir="row" alignItems="center" style={{ gap: scale(20) }}>
+                <TouchableOpacity onPress={handleBackPress}>
+                    <Image
+                        style={{ height: scale(20), width: scale(15), padding: customTheme.spacing.small }}
+                        source={require("../../assets/match/backIcon.png")}
+                    />
+                </TouchableOpacity>
+                <Image style={{ height: scale(35), width: scale(35) }} source={require("../../assets/match/logoF.png")} />
+            </Div>
             <Div position="relative">
                 <Image style={{ height: scale(27), width: scale(22) }} source={require("../../assets/match/bellInclinada.png")} />
                 <Div position="absolute" right={0} top={4} rounded={'circle'} alignItems="center" justifyContent="center" bg="orange" px={scale(3)}>
@@ -15,5 +37,5 @@ export const CustomHeader = () => {
                 </Div>
             </Div>
         </Div>
-    )
-}
+    );
+};
