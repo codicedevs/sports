0c906bf8c41fb8bf09b508ab9defda5295f9a867
StalaDev <siminicnicolas@gmail.com>
2025-03-26 17:24:49 -0300

    refactor in button

--- mobile/components/handleMatchesButton.tsx
+++ mobile/components/handleMatchesButton.tsx
@@ -5,19 +5,21 @@ import { ImageBackground, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { AppScreens } from "../navigation/screens";
 import { verticalScale } from "react-native-size-matters";
+import { useSession } from "../context/authProvider";
 
 const HandleMatchesButton = () => {
     const navigation = useNavigation()
+    const { currentUser, showModal } = useSession()
     return (
         <Div w={"100%"} h={verticalScale(150)} flexDir="row">
-            <TouchableOpacity style={{ width: '50%' }} onPress={() => navigation.navigate(AppScreens.MATCH_SCREEN)}>
+            <TouchableOpacity style={{ width: '50%' }} onPress={() => currentUser? navigation.navigate(AppScreens.MATCH_SCREEN) : showModal()}>
                 <ImageBackground style={{ justifyContent: 'center', height: "100%" }} source={require("../assets/match/InicioLeft.png")}>
                     <Div p={customTheme.spacing.medium}>
                         <Text fontFamily="NotoSans-BoldItalic" color="white" textAlign="center" fontSize={customTheme.fontSize.large} >BUSCAR{"\n"}PARTIDOS</Text>
                     </Div>
                 </ImageBackground>
             </TouchableOpacity>
-            <TouchableOpacity style={{ width: '50%' }} >
+            <TouchableOpacity onPress={() => currentUser? navigation.navigate(AppScreens.MATCH_HANDLER) : showModal()} style={{ width: '50%' }} >
                 <ImageBackground style={{ justifyContent: "center", height: "100%" }} source={require("../assets/match/InicioRight.png")}>
                     <Div p={customTheme.spacing.medium}>
                         <Text color="white" fontFamily="NotoSans-BoldItalic" textAlign="center" fontSize={customTheme.fontSize.large}>ORGANIZAR{"\n"}PARTIDOS</Text>
