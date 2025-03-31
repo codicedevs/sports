2f29b565814f47e829c660dcf1eb6c88d435e305
StalaDev <siminicnicolas@gmail.com>
2025-03-31 14:37:26 -0300

    added types

--- mobile/types/query.types.ts
+++ mobile/types/query.types.ts
@@ -1,13 +1,14 @@
 export enum QUERY_KEYS {
-    USERS = "users",
-    MATCHES = "matches",
-    PUBLIC_MATCHES = "publicMatches",
-    PETITIONS = "petitions",
-    SPORTS = "sports",
-    SPORT_MODES = "sportModes",
-    LOCATIONS = "locations",
-    MATCH = "match",
-    ZONES ="zones",
-    PLAYERS_STATUS = 'playerStatus',
-    EVENTS ="events"
-  }
+  USERS = "users",
+  MATCHES = "matches",
+  PUBLIC_MATCHES = "publicMatches",
+  PETITIONS = "petitions",
+  SPORTS = "sports",
+  SPORT_MODES = "sportModes",
+  LOCATIONS = "locations",
+  MATCH = "match",
+  ZONES = "zones",
+  PLAYERS_STATUS = 'playerStatus',
+  EVENTS = "events",
+  ACTIVITY = "activity"
+}
