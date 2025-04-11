61f613ac918117eaa939a3ef80a90a24a1a4c46a
StalaDev <siminicnicolas@gmail.com>
2025-04-11 14:46:33 -0300

    splash must check

--- mobile/navigation/appNavigator.tsx
+++ mobile/navigation/appNavigator.tsx
@@ -1,14 +1,35 @@
 import { NavigationContainer } from "@react-navigation/native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import VersionModal from "../components/modal/versionModal";
-import SplashScreen from "../screens/splash";
 import { AuthStackScreen, Principal } from "./stacks";
 import RestrictiveModal from "../components/modal/restrictiveModal";
 import { navigationRef } from "../utils/navigation";
-import { ModalContext } from "../context/modalProvider";
+
+import * as SplashScreen from "expo-splash-screen";
+import { useIsFetching } from "@tanstack/react-query";
+import { Image, View } from "react-native";
+import { Div } from "react-native-magnus";
+ SplashScreen.preventAutoHideAsync();
 
 const AppNavigator = () => {
-  const [open, setOpen] = useState(false);
+  const isFetching = useIsFetching();
+  const [appReady, setAppReady] = useState(false);
+
+  useEffect(() => {
+    if (!appReady && isFetching === 0) {
+      setAppReady(true);
+    }
+  }, [isFetching, appReady]);
+
+  if (!appReady) {
+    return (
+      <Div flex={1} alignItems="center" justifyContent="center">
+        <Image style={{width:"60%", height:"20%", resizeMode:"contain"}} source={require("../assets/logoDeballComplete.png")} />
+      </Div>
+    );
+  }
+
+  SplashScreen.hideAsync()
   return (
     <NavigationContainer ref={navigationRef}>
       <VersionModal />
