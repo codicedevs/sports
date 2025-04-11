bc453edc9d1d4604098c16be34afd9029642b660
manuvant <67987587+manuvant@users.noreply.github.com>
2025-04-11 13:34:14 -0300

    Update matchDetail.tsx

--- mobile/screens/matchDetail.tsx
+++ mobile/screens/matchDetail.tsx
@@ -127,6 +127,7 @@ const MatchDetail: React.FC<Props> = ({ navigation, route }) => {
           id: _id
         }
       })
+      showSnackBar('success', 'Has enviado la solicitud con exito!')
     } catch (e) {
       showSnackBar('error', 'Ya has enviado una petición para este partido.')
       console.log('error al enviar petition', e)
