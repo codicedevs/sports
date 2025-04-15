1724bc6ad8b7e1727360df704f06426f686cc7c7
Matías Trovant <mtrovant@gmail.com>
2025-04-15 16:11:36 -0300

    Funciona la Zona pero solo dejamos que cambie el nombre

--- api/src/zones/zones.service.ts
+++ api/src/zones/zones.service.ts
@@ -1,16 +1,16 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateZoneDto, UpdateZoneDto } from './zone.dto';
-import { Zone } from './zone.entity';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
-import { FindManyFilter } from 'filter/filter.dto';
-import { Filter, FilterResponse } from 'types/types';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateZoneDto, UpdateZoneDto } from "./zone.dto";
+import { Zone } from "./zone.entity";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, Types } from "mongoose";
+import { FindManyFilter } from "filter/filter.dto";
+import { Filter, FilterResponse } from "types/types";
 
 @Injectable()
 export class ZonesService {
   constructor(
     @InjectModel(Zone.name) private readonly zoneModel: Model<Zone>,
-  ) { }
+  ) {}
   async create(createZoneDto: CreateZoneDto): Promise<Zone> {
     const createdZone = new this.zoneModel(createZoneDto);
     return createdZone.save();
@@ -20,10 +20,9 @@ export class ZonesService {
     const results = await this.zoneModel.find(filter).exec();
     return {
       results,
-      totalCount: await this.zoneModel.countDocuments(filter).exec()
-    }
+      totalCount: await this.zoneModel.countDocuments(filter).exec(),
+    };
   }
-
 
   async findOne(id: Types.ObjectId): Promise<Zone> {
     const zone = await this.zoneModel.findById(id).exec();
@@ -34,9 +33,23 @@ export class ZonesService {
     return zone;
   }
 
+  async update(id: string, updateZoneDto: UpdateZoneDto): Promise<Zone> {
+    //Esto sirve solo para cambiar el  nombre
+    const updatedZone = await this.zoneModel
+      .findByIdAndUpdate(
+        { _id: id },
+        {
+          name: updateZoneDto.name,
+        },
+        { new: true },
+      )
+      .exec();
 
-  update(id: number, updateZoneDto: UpdateZoneDto) {
-    return `This action updates a #${id} zone`;
+    if (!updatedZone) {
+      throw new NotFoundException(`Zone with ID ${id} not found`);
+    }
+
+    return updatedZone;
   }
 
   remove(id: number) {
