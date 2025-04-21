b1adbc1db83576dcd38452c981981130d95ad5c1
falgarania <algarania.franco@gmail.com>
2025-04-21 14:30:22 -0300

    Tema admin..sin termnar

--- admin/src/theme/antdTheme.ts
+++ admin/src/theme/antdTheme.ts
@@ -7,6 +7,16 @@ export const lightColors = {
   pressed: "#fcfcfc",
   siderBack: "#f5f5f5",
 };
+
+export const darkColors = {
+  primary: "#f0f0f0",      // gris claro (textos)
+  cleary: "#2a2a2a",       // gris intermedio (header)
+  secondary: "#888888",    // gris medio para acentos
+  pressed: "#1f1f1f",       // gris más oscuro (presionado/fondo)
+  siderBack: "#1a1a1a",     // fondo sidebar
+};
+
+
 export const lightTheme: ThemeConfig = {
   token: {},
   components: {
@@ -32,4 +42,76 @@ export const lightTheme: ThemeConfig = {
       headerColor: "#ffffff",
     },
   },
+};
+
+export const darkTheme: ThemeConfig = {
+  token: {
+    colorBgBase: "#1a1a1a", 
+    colorText: "#f0f0f0",   
+    colorBgContainer: "#1a1a1a",
+
+    
+  },
+  components: {
+    Layout: {
+      triggerBg: darkColors.primary,
+      siderBg: darkColors.siderBack,
+      colorTextBase: "#f0f0f0",
+      headerBg: darkColors.cleary,
+      headerPadding: "10px 0",
+      headerHeight: "auto",
+      bodyBg: "#1a1a1a",
+      colorBgLayout: "#1a1a1a",
+      
+    },
+    Menu: {
+      subMenuItemBg: darkColors.siderBack,
+      itemBg: darkColors.siderBack,
+      itemSelectedBg: darkColors.cleary,
+      itemSelectedColor: darkColors.primary,
+      subMenuItemSelectedColor: darkColors.primary,
+      itemHoverBg: darkColors.cleary,
+    },
+    Table: {
+      headerBg: "#2a2a2a",
+      headerColor: "#ffffff",
+    },
+    Input: {
+      colorBgContainer: "#2a2a2a",
+      colorText: "#f0f0f0",
+      colorBorder: "#444444",
+      activeBorderColor: darkColors.primary,
+      colorTextPlaceholder: "#f0f0f0"
+    },
+    Select: {
+      colorBgContainer: "#2a2a2a",
+      colorText: "#f0f0f0",
+      colorBorder: "#444444",
+      selectorBg: "#2a2a2a",
+      optionSelectedBg: darkColors.primary,
+      optionSelectedColor: "#2a2a2a", 
+    },
+    Button: {
+      colorText: "#ffffff",
+      colorPrimary: "#444444",
+      colorPrimaryHover: "#3f76d1",
+      colorBgContainer: "#2a2a2a",
+      colorBorder: "#555555",
+      defaultBg: "#2a2a2a",
+      defaultColor: "#ffffff",
+      defaultBorderColor: "#555555",
+      
+    },
+    Typography: {
+      colorText: "#f0f0f0",
+    },
+    Segmented: { // mñn tarde noche
+      itemColor:  "#a6a2a2",
+      itemSelectedColor: "#f0f0f0", 
+      colorBgContainer: "#2a2a2a", 
+      colorPrimary: "#3f76d1", 
+      colorBorder: "#ffffff", 
+      
+    }
+  },
 };
