df310d78d24261ff3e51c87c31771a5ba1441dd2
CatoYuvone <otacyuvo@gmail.com>
2025-05-14 12:19:19 -0300

    [Backend] main.ts decomment protocol https

--- api/src/main.ts
+++ api/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   process.env.TZ = "America/Argentina/Buenos_Aires";
   const app = await NestFactory.create<NestExpressApplication>(
       AppModule,
-    //   protocol == "https" ? { httpsOptions: { key, cert } } : undefined
+      protocol == "https" ? { httpsOptions: { key, cert } } : undefined
   );
 
   app.useGlobalPipes(
